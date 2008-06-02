b550a18abf8d5d819af73093111cc008a494c211
brodrick <brodrick@73e435cf-5d48-0410-ba38-3de2b9e16419>
2008-06-02 12:49:36 +0000

    Add another reality check for corrupt data - if rainfall is negative.

--- atnf/atoms/mon/datasource/DataSourceWH1081.java
+++ atnf/atoms/mon/datasource/DataSourceWH1081.java
@@ -214,136 +214,140 @@ public class DataSourceWH1081
           String line = stdInput.readLine();
           line = stdInput.readLine();
           line = stdInput.readLine();
-          int thisinterval=Integer.parseInt(line.substring(20,line.length()).trim());
-          if (thisinterval!=lastinterval) {
-            line = stdInput.readLine();
-            Float in_humid=new Float(line.substring(20,line.length()).trim());
-            line = stdInput.readLine();
-            Float out_humid=new Float(line.substring(20,line.length()).trim());
-            if (out_humid.floatValue()>100) {
-              System.err.println("DataSourceWH1081: Invalid data..");
-              throw new Exception("Outside humidity out of range");
-            }
-            line = stdInput.readLine();
-            Float in_temp=new Float(line.substring(20,line.length()).trim());
-            line = stdInput.readLine();
-            Float out_temp=new Float(line.substring(20,line.length()).trim());
-            //Check for invalid values
-            if (out_temp.floatValue()>3000) {
-              System.err.println("DataSourceWH1081: Invalid data..");
-              throw new Exception("Outside temperature out of range");
-            }
-            line = stdInput.readLine();
-            Float wind_avg=new Float(Float.parseFloat(line.substring(20,line.length()).trim())*3.6);
-            line = stdInput.readLine();
-            Float wind_gust=new Float(Float.parseFloat(line.substring(20,line.length()).trim())*3.6);
-            line = stdInput.readLine();
-            String wind_dir_temp=line.substring(20,line.length()).trim();
-            double wdir;
-            if (wind_dir_temp.equals("N")) wdir=0;
-            else if (wind_dir_temp.equals("NNE")) wdir=22.5;
-            else if (wind_dir_temp.equals("NE"))  wdir=45.0;
-            else if (wind_dir_temp.equals("ENE")) wdir=67.5;
-            else if (wind_dir_temp.equals("E"))   wdir=90.0;
-            else if (wind_dir_temp.equals("ESE")) wdir=112.5;
-            else if (wind_dir_temp.equals("SE"))  wdir=135.0;
-            else if (wind_dir_temp.equals("SSE")) wdir=157.5;
-            else if (wind_dir_temp.equals("S"))   wdir=180.0;
-            else if (wind_dir_temp.equals("SSW")) wdir=202.5;
-            else if (wind_dir_temp.equals("SW"))  wdir=225.0;
-            else if (wind_dir_temp.equals("WSW")) wdir=247.5;
-            else if (wind_dir_temp.equals("W"))   wdir=270.0;
-            else if (wind_dir_temp.equals("WNW")) wdir=295.2;
-            else if (wind_dir_temp.equals("NW"))  wdir=315.0;
-            else wdir=337.5;
-            //Apply any wind direction correction
-            wdir=wdir+itsWindRotation;
-            if (wdir<0) wdir=360+wdir;
-            if (wdir>360) wdir=wdir-360;
-            Float wind_dir=new Float(wdir);
-            line = stdInput.readLine();
-            line = stdInput.readLine();
-            Float rain=new Float(line.substring(20,line.length()).trim());
-            lastrain=rain;
-            if (rain9am==null) {
-              System.err.println("DataSourceWH1081: 9am rainfall not set yet");
-              throw new Exception("DataSourceWH1081: 9am rainfall not set yet");
-            }
-            rain=new Float(itsRainScale*(rain.floatValue()-rain9am.floatValue()));
-            line = stdInput.readLine();
-            line = stdInput.readLine();
-            line = stdInput.readLine();
-            float pres_temp=Float.parseFloat(line.substring(20,line.length()).trim());
-            Float pres=new Float(pres_temp+itsPresOffset);
-        
-            if (wind_gust.floatValue()>itsWindMax) itsWindMax=wind_gust.floatValue();
-            itsWindSum+=wind_avg.floatValue();
-            itsDirSum+=wdir+360;
-            itsWindCount++;    
-
-            PointMonitor pm = MonitorMap.getPointMonitor(itsSource+"."+itsTreeBase+".in_temp");
-            if (pm!=null) {
-              PointData pd=new PointData(itsTreeBase+".in_temp", itsSource, in_temp);
-              pm.firePointEvent(new PointEvent(this, pd, true));
-            }
-            pm = MonitorMap.getPointMonitor(itsSource+"."+itsTreeBase+".out_temp");
-            if (pm!=null) {
-              PointData pd=new PointData(itsTreeBase+".out_temp", itsSource, out_temp);
-              pm.firePointEvent(new PointEvent(this, pd, true));
-            }
-
-            pm = MonitorMap.getPointMonitor(itsSource+"."+itsTreeBase+".in_humid");
-            if (pm!=null) {
-              PointData pd=new PointData(itsTreeBase+".in_humid", itsSource, in_humid);
-              pm.firePointEvent(new PointEvent(this, pd, true));
-            }
-            pm = MonitorMap.getPointMonitor(itsSource+"."+itsTreeBase+".out_humid");
-            if (pm!=null) {
-              PointData pd=new PointData(itsTreeBase+".out_humid", itsSource, out_humid);
-              pm.firePointEvent(new PointEvent(this, pd, true));
-            }
-            pm = MonitorMap.getPointMonitor(itsSource+"."+itsTreeBase+".pressure");
-            if (pm!=null) {
-              PointData pd=new PointData(itsTreeBase+".pressure", itsSource, pres);
-              pm.firePointEvent(new PointEvent(this, pd, true));
-            }
-            pm = MonitorMap.getPointMonitor(itsSource+"."+itsTreeBase+".wind_dir");
-            if (pm!=null) {
-              PointData pd=new PointData(itsTreeBase+".wind_gust_dir", itsSource, wind_dir);
-              pm.firePointEvent(new PointEvent(this, pd, true));
-            }
-            pm = MonitorMap.getPointMonitor(itsSource+"."+itsTreeBase+".wind_gust_speed");
-            if (pm!=null) {
-              PointData pd=new PointData(itsTreeBase+".wind_gust_speed", itsSource, wind_gust);
-              pm.firePointEvent(new PointEvent(this, pd, true));
-            }
-            pm = MonitorMap.getPointMonitor(itsSource+"."+itsTreeBase+".wind_avg_speed");
-            if (pm!=null) {
-              PointData pd=new PointData(itsTreeBase+".wind_avg_speed", itsSource, wind_avg);
-              pm.firePointEvent(new PointEvent(this, pd, true));
-            }
-            pm = MonitorMap.getPointMonitor(itsSource+"."+itsTreeBase+".rain");
-            if (pm!=null) {
-              PointData pd=new PointData(itsTreeBase+".rain", itsSource, rain);
-              pm.firePointEvent(new PointEvent(this, pd, true));
-            }
-            pm = MonitorMap.getPointMonitor(itsSource+"."+itsTreeBase+".wind_gust_15m");
-            if (pm!=null) {
-              PointData pd=new PointData(itsTreeBase+".wind_gust_15m", itsSource, new Float(itsWindMaxL));
-              pm.firePointEvent(new PointEvent(this, pd, true));
-            }
-            pm = MonitorMap.getPointMonitor(itsSource+"."+itsTreeBase+".wind_avg_15m");
-            if (pm!=null) {
-              PointData pd=new PointData(itsTreeBase+".wind_avg_15m", itsSource, new Float(itsWindAvgL));
-              pm.firePointEvent(new PointEvent(this, pd, true));
-            }            
-            pm = MonitorMap.getPointMonitor(itsSource+"."+itsTreeBase+".wind_dir_15m");
-            if (pm!=null) {
-              PointData pd=new PointData(itsTreeBase+".wind_dir_15m", itsSource, new Float(itsDirAvgL));
-              pm.firePointEvent(new PointEvent(this, pd, true));
-            } 
-            lastinterval=thisinterval;           
+          line = stdInput.readLine();
+          Float in_humid=new Float(line.substring(20,line.length()).trim());
+          line = stdInput.readLine();
+          Float out_humid=new Float(line.substring(20,line.length()).trim());
+          if (out_humid.floatValue()>100) {
+            System.err.println("DataSourceWH1081: Invalid data..");
+            throw new Exception("Outside humidity out of range");
           }
+          line = stdInput.readLine();
+          Float in_temp=new Float(line.substring(20,line.length()).trim());
+          //Check for invalid values
+          if (in_temp.floatValue()>80 || in_temp.floatValue()<-20) {
+            System.err.println("DataSourceWH1081: Invalid data..");
+            throw new Exception("Inside temperature out of range");
+          }
+          line = stdInput.readLine();
+          Float out_temp=new Float(line.substring(20,line.length()).trim());
+          //Check for invalid values
+          if (out_temp.floatValue()>80 || out_temp.floatValue()<-20) {
+            System.err.println("DataSourceWH1081: Invalid data..");
+            throw new Exception("Outside temperature out of range");
+          }
+          line = stdInput.readLine();
+          Float wind_avg=new Float(Float.parseFloat(line.substring(20,line.length()).trim())*3.6);
+          line = stdInput.readLine();
+          Float wind_gust=new Float(Float.parseFloat(line.substring(20,line.length()).trim())*3.6);
+          line = stdInput.readLine();
+          String wind_dir_temp=line.substring(20,line.length()).trim();
+          double wdir;
+          if (wind_dir_temp.equals("N")) wdir=0;
+          else if (wind_dir_temp.equals("NNE")) wdir=22.5;
+          else if (wind_dir_temp.equals("NE"))  wdir=45.0;
+          else if (wind_dir_temp.equals("ENE")) wdir=67.5;
+          else if (wind_dir_temp.equals("E"))   wdir=90.0;
+          else if (wind_dir_temp.equals("ESE")) wdir=112.5;
+          else if (wind_dir_temp.equals("SE"))  wdir=135.0;
+          else if (wind_dir_temp.equals("SSE")) wdir=157.5;
+          else if (wind_dir_temp.equals("S"))   wdir=180.0;
+          else if (wind_dir_temp.equals("SSW")) wdir=202.5;
+          else if (wind_dir_temp.equals("SW"))  wdir=225.0;
+          else if (wind_dir_temp.equals("WSW")) wdir=247.5;
+          else if (wind_dir_temp.equals("W"))   wdir=270.0;
+          else if (wind_dir_temp.equals("WNW")) wdir=295.2;
+          else if (wind_dir_temp.equals("NW"))  wdir=315.0;
+          else wdir=337.5;
+          //Apply any wind direction correction
+          wdir=wdir+itsWindRotation;
+          if (wdir<0) wdir=360+wdir;
+          if (wdir>360) wdir=wdir-360;
+          Float wind_dir=new Float(wdir);
+          line = stdInput.readLine();
+          line = stdInput.readLine();
+          Float rain=new Float(line.substring(20,line.length()).trim());
+          lastrain=rain;
+          if (rain9am==null) {
+            System.err.println("DataSourceWH1081: 9am rainfall not set yet");
+            throw new Exception("DataSourceWH1081: 9am rainfall not set yet");
+          }
+          if (rain.floatValue()<rain9am.floatValue()) {
+            System.err.println("DataSourceWH1081: rain reset since 9am");
+            throw new Exception("DataSourceWH1081: rain reset since 9am");
+          }
+          rain=new Float(itsRainScale*(rain.floatValue()-rain9am.floatValue()));
+          line = stdInput.readLine();
+          line = stdInput.readLine();
+          line = stdInput.readLine();
+          float pres_temp=Float.parseFloat(line.substring(20,line.length()).trim());
+          Float pres=new Float(pres_temp+itsPresOffset);
+      
+          if (wind_gust.floatValue()>itsWindMax) itsWindMax=wind_gust.floatValue();
+          itsWindSum+=wind_avg.floatValue();
+          itsDirSum+=wdir+360;
+          itsWindCount++;    
+
+          PointMonitor pm = MonitorMap.getPointMonitor(itsSource+"."+itsTreeBase+".in_temp");
+          if (pm!=null) {
+            PointData pd=new PointData(itsTreeBase+".in_temp", itsSource, in_temp);
+            pm.firePointEvent(new PointEvent(this, pd, true));
+          }
+          pm = MonitorMap.getPointMonitor(itsSource+"."+itsTreeBase+".out_temp");
+          if (pm!=null) {
+            PointData pd=new PointData(itsTreeBase+".out_temp", itsSource, out_temp);
+            pm.firePointEvent(new PointEvent(this, pd, true));
+          }
+          pm = MonitorMap.getPointMonitor(itsSource+"."+itsTreeBase+".in_humid");
+          if (pm!=null) {
+          PointData pd=new PointData(itsTreeBase+".in_humid", itsSource, in_humid);
+          pm.firePointEvent(new PointEvent(this, pd, true));
+          }
+          pm = MonitorMap.getPointMonitor(itsSource+"."+itsTreeBase+".out_humid");
+          if (pm!=null) {
+            PointData pd=new PointData(itsTreeBase+".out_humid", itsSource, out_humid);
+            pm.firePointEvent(new PointEvent(this, pd, true));
+          }
+          pm = MonitorMap.getPointMonitor(itsSource+"."+itsTreeBase+".pressure");
+          if (pm!=null) {
+            PointData pd=new PointData(itsTreeBase+".pressure", itsSource, pres);
+            pm.firePointEvent(new PointEvent(this, pd, true));
+          }
+          pm = MonitorMap.getPointMonitor(itsSource+"."+itsTreeBase+".wind_dir");
+          if (pm!=null) {
+            PointData pd=new PointData(itsTreeBase+".wind_gust_dir", itsSource, wind_dir);
+            pm.firePointEvent(new PointEvent(this, pd, true));
+          }
+          pm = MonitorMap.getPointMonitor(itsSource+"."+itsTreeBase+".wind_gust_speed");
+          if (pm!=null) {
+            PointData pd=new PointData(itsTreeBase+".wind_gust_speed", itsSource, wind_gust);
+            pm.firePointEvent(new PointEvent(this, pd, true));
+          }
+          pm = MonitorMap.getPointMonitor(itsSource+"."+itsTreeBase+".wind_avg_speed");
+          if (pm!=null) {
+            PointData pd=new PointData(itsTreeBase+".wind_avg_speed", itsSource, wind_avg);
+            pm.firePointEvent(new PointEvent(this, pd, true));
+          }
+          pm = MonitorMap.getPointMonitor(itsSource+"."+itsTreeBase+".rain");
+          if (pm!=null) {
+            PointData pd=new PointData(itsTreeBase+".rain", itsSource, rain);
+            pm.firePointEvent(new PointEvent(this, pd, true));
+          }
+          pm = MonitorMap.getPointMonitor(itsSource+"."+itsTreeBase+".wind_gust_15m");
+          if (pm!=null) {
+            PointData pd=new PointData(itsTreeBase+".wind_gust_15m", itsSource, new Float(itsWindMaxL));
+            pm.firePointEvent(new PointEvent(this, pd, true));
+          }
+          pm = MonitorMap.getPointMonitor(itsSource+"."+itsTreeBase+".wind_avg_15m");
+          if (pm!=null) {
+            PointData pd=new PointData(itsTreeBase+".wind_avg_15m", itsSource, new Float(itsWindAvgL));
+            pm.firePointEvent(new PointEvent(this, pd, true));
+          }            
+          pm = MonitorMap.getPointMonitor(itsSource+"."+itsTreeBase+".wind_dir_15m");
+          if (pm!=null) {
+            PointData pd=new PointData(itsTreeBase+".wind_dir_15m", itsSource, new Float(itsDirAvgL));
+            pm.firePointEvent(new PointEvent(this, pd, true));
+          } 
         } catch (Exception e) {          
           System.err.println("DataSourceWH1081: " + e.getClass());
         }
