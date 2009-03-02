2938317db526fea8a9b91ae388b354361664f054
brodrick <brodrick@73e435cf-5d48-0410-ba38-3de2b9e16419>
2009-03-02 01:21:08 +0000

    Add two DataSources for working with UDP.

---
+++ atnf/atoms/mon/datasource/DataSourceUDPQuery.java
@@ -0,0 +1,107 @@
+// Copyright (C) CSIRO Australia Telescope National Facility
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+package atnf.atoms.mon.datasource;
+
+import java.net.*;
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.HashMap;
+
+import atnf.atoms.util.*;
+import atnf.atoms.time.*;
+import atnf.atoms.mon.*;
+import atnf.atoms.mon.transaction.*;
+
+
+/**
+ * This class is for when you need to send an ASCII query string to a remote
+ * UDP socket and expect an ASCII string in the response packet.
+ *
+ * <P>Monitor points using this should use a TransactionStrings, where the
+ * first non-channel string contains the query string that needs to be sent
+ * to the server.
+ *
+ * <P>If the response times out then <tt>null</tt> will be returned, otherwise
+ * the full response packet will be returned as a String. If appropriate you
+ * can use use appropriate Translations to dice and slice the response string.
+ *
+ * The monitor-sources.txt arguments for this class are:
+ * <bl>
+ * <li>Remote host name
+ * <li>Remote port number
+ * <li>Timeout (ms) - Optional
+ * </bl>
+ *
+ * The name of the resulting channel will take the format
+ * <tt>hostname-port</tt>, you will need to use this in the Transaction
+ * for any points which will use this DataSource.
+ *
+ * @author David Brodrick
+ * @version $Id: $
+ **/
+public class DataSourceUDPQuery
+extends DataSourceUDPSocket
+{
+  /** Address of the remote host. */
+  protected InetAddress itsRemoteHost;
+  
+  /** Port on the remote host to send the query to. */
+  protected int itsRemotePort;
+  
+  /** constructor. */
+  public DataSourceUDPQuery(String args[])
+  {
+    super(new String[]{args[0]+"-"+args[1]});
+    
+    try {
+      itsRemoteHost=InetAddress.getByName(args[0]);
+      itsRemotePort=Integer.parseInt(args[1]);
+    } catch (Exception e) {
+      System.err.println("DataSourceUDPQuery: Error parsing arguments:");
+      e.printStackTrace();
+    }
+    
+    //Check for the optional timeout argument
+    if (args.length>2) {
+      try {
+        itsTimeout=Integer.parseInt(args[2]);
+      } catch (Exception e) {
+        System.err.println("DataSourceUDPQuery (" + itsRemoteHost + ":" +
+                           itsRemotePort + "): Error parsing timeout argument");
+      }    
+    }
+  }
+
+
+  /** Do the query and return the result. */
+  public
+  Object
+  parseData(PointMonitor requestor)
+  throws Exception
+  {
+    try {
+      //Get the query string from the point's transaction
+      String requeststr = ((TransactionStrings)requestor.getTransaction()).getString(0);
+    
+      //Build datagrams for the request and response packet
+      DatagramPacket request = new DatagramPacket(requeststr.getBytes(), requeststr.length(),
+                                                itsRemoteHost, itsRemotePort);
+      DatagramPacket response = new DatagramPacket(new byte[1500], 1500);
+
+      //Send the query and obtain the response
+      itsSocket.send(request);
+      itsSocket.receive(response);
+
+      //Get the response packet payload as a string    
+      String res = new String(response.getData());
+      return res;
+    } catch (Exception e) {
+      return null;
+    }
+  }
+}
---
+++ atnf/atoms/mon/datasource/DataSourceUDPSocket.java
@@ -0,0 +1,145 @@
+// Copyright (C) CSIRO Australia Telescope National Facility
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+package atnf.atoms.mon.datasource;
+
+import java.net.*;
+import java.io.*;
+import atnf.atoms.mon.*;
+
+/**
+ * Abstract class which binds to a local UDP socket. The first argument must
+ * be the full 'channel' string. An optional second argument can specify the
+ * local port to bind to, if not specified then any system assigned port will
+ * be used. The third argument, also optional, is the SO timeout to associate
+ * with the socket, in milliseconds.
+ *
+ * @author David Brodrick
+ * @version $Id: $
+ **/
+public abstract class DataSourceUDPSocket
+extends DataSource
+{
+  /** The socket used for communicating with the remote service. */
+  protected DatagramSocket itsSocket = null;
+
+  /** The local port to bind to, -1 for system assigned local port. */
+  protected int itsLocalPort = -1;
+  
+  /** Read timeout period, in ms. */
+  protected int itsTimeout = 5000;
+
+  public DataSourceUDPSocket(String[] args)
+  {
+    super(args[0]);
+    
+    if (args.length>1) {
+      itsLocalPort=Integer.parseInt(args[1]);
+    }
+    if (args.length>2) {
+      itsTimeout=Integer.parseInt(args[2]);
+    }
+    
+    try {
+    connect();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  /** Set the socket timeout to use (ms). */
+  protected
+  void
+  setTimeout(int ms) {
+    itsTimeout=ms;
+    if (itsConnected) {
+      try {
+        itsSocket.setSoTimeout(itsTimeout);
+      } catch (Exception e) {
+        try {
+          disconnect();
+        } catch (Exception f) {}
+      }
+    }
+  }
+
+  /** Make a new socket connection. */
+  public
+  boolean
+  connect()
+  throws Exception
+  {
+    try {
+      if (itsLocalPort==-1) {
+        itsSocket = new DatagramSocket();
+      } else {
+        itsSocket = new DatagramSocket(itsLocalPort);
+      }
+      itsSocket.setSoTimeout(itsTimeout);
+      itsConnected = true;
+      itsNumTransactions=0;
+    } catch (Exception e) {
+      e.printStackTrace();
+      itsSocket = null;
+      itsConnected  = false;
+      throw e;
+    }
+    return itsConnected;
+  }
+
+
+  /** Close the socket, unless it is already closed. */
+  public
+  void
+  disconnect()
+  throws Exception
+  {
+    if (itsSocket!=null) itsSocket.close();
+    itsSocket = null;
+    itsConnected  = false;
+  }
+
+
+  /** Implement this method in your class. The <tt>requestor</tt> argument
+   * can be used to determine which data to request, if your implementation
+   * provides different kinds of data to different monitor points. */
+  public abstract
+  Object
+  parseData(PointMonitor requestor)
+  throws Exception;
+
+
+  /** Collect data and fire events to queued monitor points. */
+  protected
+  void
+  getData(Object[] points)
+  throws Exception
+  {
+    //Precondition
+    if (points==null || points.length==0 || !itsConnected) return;
+
+    Object[] buf = points;
+    try {
+      for (int i=0; i<buf.length; i++) {
+        Object o = null;
+        PointMonitor pm = (PointMonitor)buf[i];
+        o = parseData(pm);
+        //Count successful transactions
+        if (o!=null) itsNumTransactions += buf.length;
+        //Fire the new data off for this point
+        pm.firePointEvent(new PointEvent(this,
+           new PointData(pm.getName(), pm.getSource(), o), true));
+      }
+    } catch (Exception e) {
+      //Probably a comms error.
+      disconnect();
+      System.err.println("DataSourceUDPSocket: " + e.getClass() + ": " + e.getMessage());
+      e.printStackTrace();
+      MonitorMap.logger.error(e.getMessage());
+    }
+  }
+}
