537e29982e798683306b40901c16ad2821afda6a
brodrick <brodrick@73e435cf-5d48-0410-ba38-3de2b9e16419>
2009-03-31 04:42:55 +0000

    Remove redundant files. Move build and key files to top level.

--- trunk/bin/startMoniCA.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-NUM=$(ps -efw | grep "java -jar open-monica.jar" | grep -v grep | wc -l)
-
-if (( $NUM < 1 )); then
-  cd ~/open-monica-read-only
-  java -jar open-monica.jar >/dev/null 2>/dev/null
-fi
