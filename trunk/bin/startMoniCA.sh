14ebd65652f56c8a3e357bff856e31e344f622c1
brodrick <brodrick@73e435cf-5d48-0410-ba38-3de2b9e16419>
2009-03-31 04:37:26 +0000

    Modify build file to support install target and autogenerate start/stop scripts.

--- trunk/bin/startMoniCA.sh
+++ trunk/bin/startMoniCA.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-NUM=$(ps -efw | grep "java -jar monica-server" | grep -v grep | wc -l)
+NUM=$(ps -efw | grep "java -jar open-monica.jar" | grep -v grep | wc -l)
 
 if (( $NUM < 1 )); then
   cd ~/open-monica-read-only
-  java -jar monica-server.jar >/dev/null 2>/dev/null
+  java -jar open-monica.jar >/dev/null 2>/dev/null
 fi
