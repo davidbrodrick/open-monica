b70f70fc6b54bc6781591f3e628e281883f59ff4
brodrick <brodrick@73e435cf-5d48-0410-ba38-3de2b9e16419>
2010-02-25 03:45:59 +0000

    Add DavisVantagePro support - untested.

--- trunk/src/atnf/atoms/mon/externalsystem/DavisVantagePro.java
+++ trunk/src/atnf/atoms/mon/externalsystem/DavisVantagePro.java
@@ -12,19 +12,28 @@ import java.util.HashMap;
 import atnf.atoms.mon.*;
 
 /**
- * Pulls data from a Davis Weather Monitor II weather station. Data is published as a
- * HashMap with the following keys defined:
+ * Pulls data from a Davis Vantage Pro or Vantage Pro 2 weather station. Data is published
+ * as a HashMap with the following keys defined:
  * 
  * <P>
  * <bl>
  * <li><b>OUTTEMP</b> Outside temperature in degrees Celcius.
  * <li><b>INTEMP</b> Inside temperature in degrees Celcius.
+ * <li><b>EXTEMPx</b> Extra temperature sensor in degrees Celcius, x=1-7.
+ * <li><b>SOILTEMPx</b> Soil temperature sensor in degrees Celcius, x=1-4.
+ * <li><b>LEAFTEMPx</b> Leaf temperature sensor in degrees Celcius, x=1-4.
  * <li><b>OUTHUMID</b> Outside relative humidity as a percentage.
  * <li><b>INHUMID</b> Inside relative humidity as a percentage.
+ * <li><b>EXHUMIDx</b> Extra relative humidity sensor as a percentage, x=1-7.
+ * <li><b>SOILMOISTx</b> Soil moisture in centibar, x=1-4.
+ * <li><b>LEAFWETx</b> Leaf wetness, x=1-4.
  * <li><b>RAIN</b> Total number of rainfall tips.
  * <li><b>PRES</b> Atmospheric pressure.
  * <li><b>WINDSPD</b> Wind speed in km/h.
- * <li><b>WINDDIR</b> Wind direction in degrees of azimuth. </bl>
+ * <li><b>WINDDIR</b> Wind direction in degrees of azimuth.
+ * <li><b>SOLRAD</b> Solar radiation, in Watts per square metre.
+ * <li><b>UV</b> Ultravoilet radiation, in UV Index units.
+ * <li><b>BATTVOLTS</b> Battery voltage, in Volts. </bl>
  * 
  * <P>
  * The <tt>monitor-sources.txt</tt> file needs the hostname and port number of the TCP
@@ -32,19 +41,28 @@ import atnf.atoms.mon.*;
  * 
  * <P>
  * <tt>
- * DavisWeatherMonitorII 130.155.177.158:10000
+ * DavisVantagePro 130.155.177.158:10000
  * </tt>
  * 
- * An example set of monitor point definitions is shown below: <tt>
+ * An basic set of monitor point definitions is shown below, these can be expanded using
+ * the definitions above:
+ * 
+ * <P>
+ * <tt>
  * #Full set of weather data read from the weather station
  * hidden.davis.data           "Raw weather data"            ""            ""     site T Generic-"130.155.177.158:10000"  -  -  -  - 1500000  -
  * ##Weather data direct from weather station
  * weather.OutTemp             "Outside Temperature"         "Out Temp"    "C"    site T Listen-"$1.hidden.davis.data"     -  {NV-"OUTTEMP", Mean-"60", NumDecimals-"1"}  -             {TIMER-60}           1500000  -
  * weather.InTemp              "Inside Temperature"          "In Temp"     "C"    site T Listen-"$1.hidden.davis.data"     -  {NV-"INTEMP", Mean-"60", NumDecimals-"1"}   -             {TIMER-60}           1500000  -
+ * weather.ExtraTemp1          "Extra Temperature 1"         "Ex Temp 1"   "C"    site T Listen-"$1.hidden.davis.data"     -  {NV-"EXTEMP1", Mean-"60", NumDecimals-"1"}  -             {TIMER-60}           1500000  -
  * weather.OutHumid            "Outside Relative Humidity"   "Out Humid"   "%"    site T Listen-"$1.hidden.davis.data"     -  {NV-"OUTHUMID", Mean-"60", NumDecimals-"1"} -             {TIMER-60}           1500000  -
  * weather.InHumid             "Inside Relative Humidity"    "In Humid"    "%"    site T Listen-"$1.hidden.davis.data"     -  {NV-"INHUMID", Mean-"60", NumDecimals-"1"}  -             {TIMER-60}           1500000  -
+ * weather.ExtraHumid1         "Extra Relative Humidity 1"   "Ex Humid 1"  "%"    site T Listen-"$1.hidden.davis.data"     -  {NV-"EXHUMID1", Mean-"60", NumDecimals-"1"} -             {TIMER-60}           1500000  -
  * weather.Pressure            "Atmospheric Pressure"        "Pressure"    "hPa"  site T Listen-"$1.hidden.davis.data"     -  {NV-"PRES", Mean-"60", NumDecimals-"1"}     -             {TIMER-180}          1500000  -
- * weather.Wind                "Wind Speed"                  "Wind Spd"    "km/h" site T Listen-"$1.hidden.davis.data"     -  {NV-"WINDSPD"}                              Range-"0""45" {COUNTER-1}          1500000  2
+ * weather.SolarRad            "Solar Radiation"             "Solar Rad"  "W/m^2" site T Listen-"$1.hidden.davis.data"     -  {NV-"SOLRAD", NumDecimals-"1"}              -             {TIMER-180}          1500000  -
+ * weather.UV                  "Ultraviolet Radiation"       "UV Rad"      ""     site T Listen-"$1.hidden.davis.data"     -  {NV-"UV", NumDecimals-"1"}                  -             {TIMER-180}          1500000  -
+ * weather.Battery             "Battery Voltage"             "Batt Volts"  "V"    site T Listen-"$1.hidden.davis.data"     -  {NV-"BATTVOLTS"}                            -             {TIMER-60}           1500000  - 
+ * weather.Wind                "Wind Speed"                  "Wind Spd"    "km/h" site T Listen-"$1.hidden.davis.data"     -  {NV-"WINDSPD"}                              Range-"0""45" {COUNTER-1}          1500000  2 
  * #NB: Need to set the angle correction for your wind vane here, 0 by default
  * weather.WindDir             "Wind Direction"              "Wind Dir"    "deg"  site T Listen-"$1.hidden.davis.data"     -  {NV-"WINDDIR", EQ-"(x+0)%360"}              -             {COUNTER-1}          1500000  2
  * weather.RainTotalTips       "Total Ever Rainfall Tips"    "Total Tips"  "tips" site T Listen-"$1.hidden.davis.data"     -  {NV-"RAIN"}                                 -             {CHANGE-, TIMER-300} 1500000  -
@@ -69,10 +87,10 @@ import atnf.atoms.mon.*;
  * 
  * @author David Brodrick
  */
-public class DavisWeatherMonitorII extends DataSocket
+public class DavisVantagePro extends DataSocket
 {
     /** Argument must include host:port and optionally :timeout_ms */
-    public DavisWeatherMonitorII(String[] args)
+    public DavisVantagePro(String[] args)
     {
         super(args);
     }
@@ -88,14 +106,45 @@ public class DavisWeatherMonitorII extends DataSocket
 
         int temp;
         // Temperatures
-        temp = rawbytes[1] + 256 * rawbytes[2];
+        temp = rawbytes[10] + 256 * rawbytes[11];
         res.put("INTEMP", new Float(5 * ((temp / 10.0) - 32) / 9.0));
-        temp = rawbytes[3] + 256 * rawbytes[4];
+        temp = rawbytes[13] + 256 * rawbytes[14];
         res.put("OUTTEMP", new Float(5 * ((temp / 10.0) - 32) / 9.0));
+        res.put("EXTEMP1", new Float(5 * ((rawbytes[19] - 90) - 32) / 9.0));
+        res.put("EXTEMP2", new Float(5 * ((rawbytes[20] - 90) - 32) / 9.0));
+        res.put("EXTEMP3", new Float(5 * ((rawbytes[21] - 90) - 32) / 9.0));
+        res.put("EXTEMP4", new Float(5 * ((rawbytes[22] - 90) - 32) / 9.0));
+        res.put("EXTEMP5", new Float(5 * ((rawbytes[23] - 90) - 32) / 9.0));
+        res.put("EXTEMP6", new Float(5 * ((rawbytes[24] - 90) - 32) / 9.0));
+        res.put("EXTEMP7", new Float(5 * ((rawbytes[25] - 90) - 32) / 9.0));
+        res.put("SOILTEMP1", new Float(5 * ((rawbytes[26] - 90) - 32) / 9.0));
+        res.put("SOILTEMP2", new Float(5 * ((rawbytes[27] - 90) - 32) / 9.0));
+        res.put("SOILTEMP3", new Float(5 * ((rawbytes[28] - 90) - 32) / 9.0));
+        res.put("SOILTEMP4", new Float(5 * ((rawbytes[29] - 90) - 32) / 9.0));
+        res.put("LEAFTEMP1", new Float(5 * ((rawbytes[30] - 90) - 32) / 9.0));
+        res.put("LEAFTEMP2", new Float(5 * ((rawbytes[31] - 90) - 32) / 9.0));
+        res.put("LEAFTEMP3", new Float(5 * ((rawbytes[32] - 90) - 32) / 9.0));
+        res.put("LEAFTEMP4", new Float(5 * ((rawbytes[33] - 90) - 32) / 9.0));
+
+        // Soil moistures, in centibar
+        res.put("SOILMOIST1", new Float(rawbytes[63]));
+        res.put("SOILMOIST2", new Float(rawbytes[64]));
+        res.put("SOILMOIST3", new Float(rawbytes[65]));
+        res.put("SOILMOIST4", new Float(rawbytes[66]));
+
+        // Leaf wetness, 0=very dry, 15=very wet
+        res.put("LEAFWET1", new Float(rawbytes[67]));
+        res.put("LEAFWET2", new Float(rawbytes[68]));
+        res.put("LEAFWET3", new Float(rawbytes[69]));
+        res.put("LEAFWET4", new Float(rawbytes[70]));
+
+        // Battery voltage
+        temp = rawbytes[88] + 256 * rawbytes[89];
+        res.put("BATTVOLTS", new Float(((temp * 300) / 512) / 100));
 
         // Wind readings
-        res.put("WINDSPD", new Float(rawbytes[5] * 1.61));
-        temp = rawbytes[6] + 256 * rawbytes[7];
+        res.put("WINDSPD", new Float(rawbytes[15] * 1.61));
+        temp = rawbytes[17] + 256 * rawbytes[18];
         res.put("WINDDIR", new Float(temp));
 
         // Barometric pressure
@@ -103,12 +152,27 @@ public class DavisWeatherMonitorII extends DataSocket
         res.put("PRES", new Float(temp / 29.5287));
 
         // Humidity
-        res.put("INHUMID", new Float(rawbytes[10]));
-        res.put("OUTHUMID", new Float(rawbytes[11]));
+        res.put("INHUMID", new Float(rawbytes[12]));
+        res.put("OUTHUMID", new Float(rawbytes[34]));
+        res.put("EXHUMID1", new Float(rawbytes[35]));
+        res.put("EXHUMID2", new Float(rawbytes[36]));
+        res.put("EXHUMID3", new Float(rawbytes[37]));
+        res.put("EXHUMID4", new Float(rawbytes[38]));
+        res.put("EXHUMID5", new Float(rawbytes[39]));
+        res.put("EXHUMID6", new Float(rawbytes[40]));
+        res.put("EXHUMID7", new Float(rawbytes[41]));
 
         // Rain
-        temp = rawbytes[12] + 256 * rawbytes[13];
+        temp = rawbytes[55] + 256 * rawbytes[56];
         res.put("RAIN", new Float(temp)); // In tips
+
+        // Solar radiation
+        temp = rawbytes[45] + 256 * rawbytes[46];
+        res.put("SOLRAD", new Float(temp)); // In Watts
+
+        // UV
+        temp = rawbytes[44];
+        res.put("UV", new Float(temp)); // In UV Index
 
         return res;
     }
@@ -139,7 +203,7 @@ public class DavisWeatherMonitorII extends DataSocket
                 }
 
                 // Read the sensor image bytes
-                int[] rawbytes = new int[18];
+                int[] rawbytes = new int[91];
                 for (int j = 0; j < rawbytes.length; j++) {
                     rawbytes[j] = itsReader.read();
                     if (rawbytes[j] == -1) {
@@ -160,8 +224,8 @@ public class DavisWeatherMonitorII extends DataSocket
             }
         } catch (Exception e) {
             disconnect();
-            System.err.println("DavisWeatherMonitorII: " + e);
-            MonitorMap.logger.error("DavisWeatherMonitorII: " + e);
+            System.err.println("DavisVantagePro: " + e);
+            MonitorMap.logger.error("DavisVantagePro: " + e);
         }
     }
 }
