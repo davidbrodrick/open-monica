60da4b00009d6a52e13030a4f5be61322dff9a70
brodrick <brodrick@73e435cf-5d48-0410-ba38-3de2b9e16419>
2010-03-02 03:25:06 +0000

    Add Simons DC bus/battery current monitoring to ThyconUPS.

--- src/atnf/atoms/mon/limit/PointLimit.java
+++ src/atnf/atoms/mon/limit/PointLimit.java
@@ -12,62 +12,52 @@ import java.lang.reflect.*;
 import atnf.atoms.mon.*;
 import atnf.atoms.mon.util.*;
 
-
 /**
  * Base-class for classes which indicate if a point is in an alarm state.
  * <P>
- * Sub-classes must implement an appropriate <i>checkLimits</i> method. 
- * <i>checkLimits</i> should set the PointData's alarm field to True when
- * an alarm condition is detected or leave the field unchanged otherwise.
- *
+ * Sub-classes must implement an appropriate <i>checkLimits</i> method. <i>checkLimits</i>
+ * should set the PointData's alarm field to True when an alarm condition is detected or
+ * leave the field unchanged otherwise.
+ * 
  * @author Le Cuong Nguyen
  * @author David Brodrick
- **/
-public abstract class
-PointLimit
-extends MonitorPolicy
+ */
+public abstract class PointLimit extends MonitorPolicy
 {
-   /** Override this. Use format fullname, shortname, argname, argtype, argname, argtype, ...
-   */
-   protected static String itsArgs[] = new String[]{"PointLimit",""};
-   
-   public static PointLimit factory(String arg)
-   {
-      if (arg.equalsIgnoreCase("null")) {
-        arg= "-";
-      }
+    public static PointLimit factory(String arg)
+    {
+        if (arg.equalsIgnoreCase("null")) {
+            arg = "-";
+        }
 
-      PointLimit result = null;
+        PointLimit result = null;
 
-      // Find the specific informations
-      String specifics = arg.substring(arg.indexOf("-") + 1);
-      String[] limitArgs = MonitorUtils.tokToStringArray(specifics);
-      // Find the type of translation
-      String type = arg.substring(0, arg.indexOf("-"));
-      if (type == "" || type == null || type.length()<1) {
-        type = "NONE";
-      }
+        // Find the specific informations
+        String specifics = arg.substring(arg.indexOf("-") + 1);
+        String[] limitArgs = MonitorUtils.tokToStringArray(specifics);
+        // Find the type of translation
+        String type = arg.substring(0, arg.indexOf("-"));
+        if (type == "" || type == null || type.length() < 1) {
+            type = "NONE";
+        }
 
-      try {
-        Constructor Limit_con = Class.forName("atnf.atoms.mon.limit.PointLimit"+type).getConstructor(new Class[]{String[].class});
-        result = (PointLimit)(Limit_con.newInstance(new Object[]{limitArgs}));
-      } catch (Exception e) {e.printStackTrace();result = new PointLimitNONE(new String[]{});}
+        try {
+            Constructor Limit_con = Class.forName("atnf.atoms.mon.limit.PointLimit" + type).getConstructor(
+                    new Class[] { String[].class });
+            result = (PointLimit) (Limit_con.newInstance(new Object[] { limitArgs }));
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = new PointLimitNONE(new String[] {});
+        }
 
-      result.setStringEquiv(arg);
-      
-      return result;
-   }
+        result.setStringEquiv(arg);
 
+        return result;
+    }
 
-   /** Check the value against the alarm criteria.
-    * @param data New data value to check. */
-   public abstract
-   void
-   checkLimits(PointData data);
-
-
-   public static String[] getArgs()
-   {
-      return itsArgs;
-   }
+    /**
+     * Check the value against the alarm criteria.
+     * @param data New data value to check.
+     */
+    public abstract void checkLimits(PointData data);
 }
