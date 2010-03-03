8ffd500007923e4e943a363555f8d0b1b027a627
brodrick <brodrick@73e435cf-5d48-0410-ba38-3de2b9e16419>
2010-03-03 03:16:29 +0000

    Rename "limits" to "alarms".

---
+++ trunk/src/atnf/atoms/mon/alarmcheck/AlarmCheckNONE.java
@@ -0,0 +1,30 @@
+//
+// Copyright (C) CSIRO Australia Telescope National Facility
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+package atnf.atoms.mon.alarmcheck;
+
+import atnf.atoms.mon.PointData;
+
+/**
+ * Trivial alarm checker which always indicates that data is okay.
+ * @author David Brodrick
+ */
+public class AlarmCheckNONE extends AlarmCheck
+{
+    /** Constructor. */
+    public AlarmCheckNONE(String[] args)
+    {
+    }
+
+    /** Always indicates that data is okay. */
+    public void checkAlarm(PointData data)
+    {
+        return;
+    }
+
+}
