c65677fbdaaf3c7be0e652d12ef0db1d40e33f2e
brodrick <brodrick@73e435cf-5d48-0410-ba38-3de2b9e16419>
2010-03-04 21:53:31 +0000

    Add javadoc for TranslationStuckValue.

--- trunk/src/atnf/atoms/mon/translation/TranslationStuckValue.java
+++ trunk/src/atnf/atoms/mon/translation/TranslationStuckValue.java
@@ -14,87 +14,90 @@ import atnf.atoms.mon.PointData;
 import atnf.atoms.mon.PointDescription;
 
 /**
- *
+ * Just return the input, unless it hasn't changed for a specified number of updates in
+ * which case a <i>null</i> value is returned.
+ * 
+ * <P>
+ * Requires one constructor argument, being the number of idenitical updates before we
+ * declare the value is "stuck".
+ * 
  * @author David Brodrick
  */
-public class
-TranslationStuckValue
-extends Translation
+public class TranslationStuckValue extends Translation
 {
-  /** Buffer containing historical data. */
-  protected Object[] itsBuffer;
-  
-  /** The number of identical updates before we declare a value is 'stuck'. */
-  protected int itsNumUpdates;
+    /** Buffer containing historical data. */
+    protected Object[] itsBuffer;
 
-  public TranslationStuckValue(PointDescription parent, String[] init)
-  {
-    super(parent, init);
+    /** The number of identical updates before we declare a value is 'stuck'. */
+    protected int itsNumUpdates;
 
-    itsNumUpdates = Integer.parseInt(init[0]);
-    itsBuffer = new Object[itsNumUpdates];
-  }
+    public TranslationStuckValue(PointDescription parent, String[] init)
+    {
+        super(parent, init);
 
-  /** Check for stuck value, flag data if it still hasn't changed. */
-  public
-  PointData
-  translate(PointData data)
-  {
-    // Rotate the data in the buffer
-    for (int i=0; i<itsNumUpdates-1; i++) {
-      itsBuffer[i] = itsBuffer[i+1];
+        itsNumUpdates = Integer.parseInt(init[0]);
+        itsBuffer = new Object[itsNumUpdates];
     }
-    itsBuffer[itsNumUpdates-1] = data.getData();
-    
-    boolean somenull = false;
-    boolean somenotnull = false;
-    Vector<String> dataclasses = new Vector<String>();
-    for (int i=0; i<itsNumUpdates; i++) {
-      if (itsBuffer[i]==null) {
-        somenull = true;
-      } else {
-        somenotnull = true;
-        String thisclass = itsBuffer[i].getClass().getName();
-        if (!dataclasses.contains(thisclass)) {
-          dataclasses.add(thisclass);
+
+    /** Check for stuck value, flag data if it still hasn't changed. */
+    public PointData translate(PointData data)
+    {
+        // Rotate the data in the buffer
+        for (int i = 0; i < itsNumUpdates - 1; i++) {
+            itsBuffer[i] = itsBuffer[i + 1];
         }
-      }
-    }
-    
-    if (somenull && somenotnull) {
-      // Value has obviously changed to/from null
-      return data;
-    } else if (somenull && !somenotnull) {
-      // Value is stuck at null
-      return new PointData(itsParent.getFullName(), data.getTimestamp(), null, true);
-    } else if (dataclasses.size()>1) {
-      // Different classes must mean data is changing
-      return data;
-    } else {
-      // Need to check actual values, we know they are all of same class
-      if (itsBuffer[0] instanceof Number) {
-        double firstval = ((Number)itsBuffer[0]).doubleValue();
-        for (int i=1; i<itsNumUpdates; i++) {
-          if (((Number)itsBuffer[i]).doubleValue() != firstval) {
-            // Value changed
+        itsBuffer[itsNumUpdates - 1] = data.getData();
+
+        boolean somenull = false;
+        boolean somenotnull = false;
+        Vector<String> dataclasses = new Vector<String>();
+        for (int i = 0; i < itsNumUpdates; i++) {
+            if (itsBuffer[i] == null) {
+                somenull = true;
+            } else {
+                somenotnull = true;
+                String thisclass = itsBuffer[i].getClass().getName();
+                if (!dataclasses.contains(thisclass)) {
+                    dataclasses.add(thisclass);
+                }
+            }
+        }
+
+        if (somenull && somenotnull) {
+            // Value has obviously changed to/from null
             return data;
-          }
-        }
-      } else if (itsBuffer[0] instanceof String) {
-        String firstval = ((String)itsBuffer[0]);
-        for (int i=1; i<itsNumUpdates; i++) {
-          if (!((String)itsBuffer[i]).equals(firstval)) {
-            // Value changed
+        } else if (somenull && !somenotnull) {
+            // Value is stuck at null
+            return new PointData(itsParent.getFullName(), data.getTimestamp(), null, true);
+        } else if (dataclasses.size() > 1) {
+            // Different classes must mean data is changing
             return data;
-          }
+        } else {
+            // Need to check actual values, we know they are all of same class
+            if (itsBuffer[0] instanceof Number) {
+                double firstval = ((Number) itsBuffer[0]).doubleValue();
+                for (int i = 1; i < itsNumUpdates; i++) {
+                    if (((Number) itsBuffer[i]).doubleValue() != firstval) {
+                        // Value changed
+                        return data;
+                    }
+                }
+            } else if (itsBuffer[0] instanceof String) {
+                String firstval = ((String) itsBuffer[0]);
+                for (int i = 1; i < itsNumUpdates; i++) {
+                    if (!((String) itsBuffer[i]).equals(firstval)) {
+                        // Value changed
+                        return data;
+                    }
+                }
+            } else {
+                System.err.println("TranslationStuckValue(" + itsParent.getFullName() + "): Unsupported data class "
+                        + itsBuffer[0].getClass().getName());
+                return data;
+            }
         }
-      } else {
-        System.err.println("TranslationStuckValue(" + itsParent.getFullName() + "): Unsupported data class " + itsBuffer[0].getClass().getName());
-        return data;
-      }
+
+        // Value is stuck so mask to null and set alarm condition
+        return new PointData(itsParent.getFullName(), data.getTimestamp(), null, true);
     }
-    
-    // Value is stuck so mask to null and set alarm condition 
-    return new PointData(itsParent.getFullName(), data.getTimestamp(), null, true);
-  }
 }
