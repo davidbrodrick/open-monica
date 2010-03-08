3e084dc7fb3d70b12e1c7bd8c5bc5a09beb89c58
brodrick <brodrick@73e435cf-5d48-0410-ba38-3de2b9e16419>
2010-03-08 04:42:00 +0000

    Define two new archive policy classes.

---
+++ trunk/src/atnf/atoms/mon/archivepolicy/ArchivePolicyAlarm.java
@@ -0,0 +1,28 @@
+// Copyright (C) CSIRO Australia Telescope National Facility
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+
+package atnf.atoms.mon.archivepolicy;
+
+import atnf.atoms.mon.*;
+
+/**
+ * Archives data when the alarm flag is set.
+ * 
+ * @author David Brodrick
+ */
+public class ArchivePolicyAlarm extends ArchivePolicy
+{
+    public ArchivePolicyAlarm(String args)
+    {
+    }
+
+    public boolean checkArchiveThis(PointData data)
+    {
+        return data.getAlarm();
+    }
+}
