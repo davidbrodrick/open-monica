fb284f026325da79e57c5c54e2ccd376658f9179
brodrick <brodrick@73e435cf-5d48-0410-ba38-3de2b9e16419>
2010-03-08 04:42:00 +0000

    Define two new archive policy classes.

---
+++ src/atnf/atoms/mon/archivepolicy/ArchivePolicyAll.java
@@ -0,0 +1,28 @@
+// Copyright (C) CSIRO Australia Telescope National Facility
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+
+package atnf.atoms.mon.archivepolicy;
+
+import atnf.atoms.mon.*;
+
+/**
+ * Archives every data update.
+ * 
+ * @author David Brodrick
+ */
+public class ArchivePolicyAll extends ArchivePolicy
+{
+    public ArchivePolicyAll(String args)
+    {
+    }
+
+    public boolean checkArchiveThis(PointData data)
+    {
+        return true;
+    }
+}
