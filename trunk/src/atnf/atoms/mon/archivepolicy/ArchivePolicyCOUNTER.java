bfaae10e005588e075eaee38add653cca3d674f6
brodrick <brodrick@73e435cf-5d48-0410-ba38-3de2b9e16419>
2010-05-10 06:12:29 +0000

    Add some features to ArchivePolicyCHANGE. Changes to NUTUPS support.

--- trunk/src/atnf/atoms/mon/archivepolicy/ArchivePolicyCOUNTER.java
+++ trunk/src/atnf/atoms/mon/archivepolicy/ArchivePolicyCOUNTER.java
@@ -8,36 +8,34 @@ package atnf.atoms.mon.archivepolicy;
 
 import atnf.atoms.mon.*;
 
-public class ArchivePolicyCOUNTER extends ArchivePolicy
-{
-   private int itsCycles = 0;
-   private int itsRunningCycles = 0;
-   protected static String itsArgs[] = new String[]{"Counter","COUNTER","Count","java.lang.Integer"};
-   
-   public ArchivePolicyCOUNTER(String args)
-   {
-      itsCycles = Integer.parseInt(args);
-   }
-   
-   public ArchivePolicyCOUNTER(int cycles)
-   {
-      itsCycles = cycles;
-   }
-   
-   public boolean checkArchiveThis(PointData data)
-   {
-      itsRunningCycles++;
-      if (itsRunningCycles >= itsCycles) {
-         itsSaveNow = true;
-	 itsRunningCycles = 0;
-      } else {
-        itsSaveNow = false;
-      }
-      return itsSaveNow;
-   }
+public class ArchivePolicyCOUNTER extends ArchivePolicy {
+  private int itsCycles = 0;
 
-   public static String[] getArgs()
-   {
-      return itsArgs;
-   }
+  private int itsRunningCycles = 0;
+
+  protected static String itsArgs[] = new String[] { "Counter", "COUNTER", "Count", "java.lang.Integer" };
+
+  public ArchivePolicyCOUNTER(String args) {
+    args = args.replace("\"", "");
+    itsCycles = Integer.parseInt(args);
+  }
+
+  public ArchivePolicyCOUNTER(int cycles) {
+    itsCycles = cycles;
+  }
+
+  public boolean checkArchiveThis(PointData data) {
+    itsRunningCycles++;
+    if (itsRunningCycles >= itsCycles) {
+      itsSaveNow = true;
+      itsRunningCycles = 0;
+    } else {
+      itsSaveNow = false;
+    }
+    return itsSaveNow;
+  }
+
+  public static String[] getArgs() {
+    return itsArgs;
+  }
 }
