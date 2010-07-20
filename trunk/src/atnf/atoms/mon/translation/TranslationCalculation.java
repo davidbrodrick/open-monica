85e5edeca3f09aec30ee47e20c2883b4e9f5acd6
brodrick <brodrick@73e435cf-5d48-0410-ba38-3de2b9e16419>
2010-07-20 23:22:25 +0000

    Update Calculation translations to supports new JEP features.

--- trunk/src/atnf/atoms/mon/translation/TranslationCalculation.java
+++ trunk/src/atnf/atoms/mon/translation/TranslationCalculation.java
@@ -12,87 +12,81 @@ package atnf.atoms.mon.translation;
 import org.nfunk.jep.JEP;
 
 import atnf.atoms.mon.PointDescription;
+import atnf.atoms.util.Angle;
 
 /**
  * Calculate an output value which is a function one of one or more listened-to
- * points. Expression parsing is performed using JEP, the Java Expression 
+ * points. Expression parsing is performed using JEP, the Java Expression
  * Parser, which makes this Translation highly flexible and very useful.
  * <P>
  * The first constructor <i>init</i> argument needs to specify the number, N,
- * of monitor points which will be listened to. The next N arguments must be
- * the names of the points being listened to. The final argument is the
- * expression to be evaluated where each input variable is named a, b, c, etc,
+ * of monitor points which will be listened to. The next N arguments must be the
+ * names of the points being listened to. The final argument is the expression
+ * to be evaluated where each input variable is named a, b, c, etc,
  * corresponding to the order they were named.
  * <P>
- * This implementation clears all inputs after a value is calculated, so that
- * a subsequent calculation will not happen until all inputs have updated again.
- *
+ * This implementation clears all inputs after a value is calculated, so that a
+ * subsequent calculation will not happen until all inputs have updated again.
+ * 
  * @author David Brodrick
  */
-public class
-TranslationCalculation
-extends TranslationListener
+public class TranslationCalculation extends TranslationListener
 {
-  protected static String[] itsArgs = new String[]{"Calculation",
-  "Listens to two other points",
-  "NumPoints", "Integer",
-  "MonitorPoint 1", "String",
-  "MonitorPoint N", "String",
-  "Function", "String"};
+  protected static String[] itsArgs = new String[] { "Calculation", "Listens to two other points", "NumPoints", "Integer",
+      "MonitorPoint 1", "String", "MonitorPoint N", "String", "Function", "String" };
 
   /** Used for parsing and evaluating the expression. */
   JEP itsParser = new JEP();
 
-  public
-  TranslationCalculation(PointDescription parent, String[] init)
+  public TranslationCalculation(PointDescription parent, String[] init)
   {
     super(parent, init);
 
-    if (init.length<itsNumPoints+2) {
+    if (init.length < itsNumPoints + 2) {
       System.err.println("TranslationCalculation: INSUFFICIENT ARGUMENTS: for " + parent.getFullName());
       return;
     }
 
-    //Configure parser options
+    // Configure parser options
     itsParser.setAllowUndeclared(true);
-    //itsParser.addStandardConstants();
+    // itsParser.addStandardConstants();
     itsParser.addStandardFunctions();
-    itsParser.parseExpression(init[itsNumPoints+1]);
+    itsParser.parseExpression(init[itsNumPoints + 1]);
   }
 
-
-  /** Provide the current input values to the expression parser, calculate
-   * result and clear all input variables so that fresh values will be used
-   * next time. */
-  protected
-  Object
-  doCalculations()
+  /**
+   * Provide the current input values to the expression parser, calculate result
+   * and clear all input variables so that fresh values will be used next time.
+   */
+  protected Object doCalculations()
   {
-    for (int i=0; i<itsNumPoints; i++) {
-      //Update the value for this variable
-      String thisvar=""+((char)(('a')+i));
-      if (!(itsValues[i].getData() instanceof Boolean)) {
-        itsParser.addVariable(thisvar,itsValues[i].getData());
-      } else {
-        boolean boolval = ((Boolean)itsValues[i].getData()).booleanValue();
-        if (boolval) {
+    for (int i = 0; i < itsNumPoints; i++) {
+      // Update the value for this variable
+      String thisvar = "" + ((char) (('a') + i));
+      Object thisval = itsValues[i].getData();
+      if (thisval instanceof Boolean) {
+        if (((Boolean) thisval).booleanValue()) {
           itsParser.addVariable(thisvar, 1.0);
         } else {
           itsParser.addVariable(thisvar, 0.0);
         }
+      } else if (thisval instanceof Angle) {
+        itsParser.addVariable(thisvar, ((Angle) thisval).getValue());
+      } else {
+        itsParser.addVariable(thisvar, thisval);
       }
-      //Clear the current value now that it has been used
-      itsValues[i]=null;
+      // Clear the current value now that it has been used
+      itsValues[i] = null;
     }
 
-    //Parse the expression using new values
-    Object res=itsParser.getValueAsObject();
-    
-    //Check for parse error
+    // Parse the expression using new values
+    Object res = itsParser.getValueAsObject();
+
+    // Check for parse error
     if (itsParser.hasError()) {
       System.err.println("TranslationCalculator(" + itsParent.getFullName() + ": " + itsParser.getErrorInfo());
     }
-    
+
     return res;
   }
 }
--- trunk/src/atnf/atoms/mon/translation/TranslationEQ.java
+++ trunk/src/atnf/atoms/mon/translation/TranslationEQ.java
@@ -10,110 +10,82 @@ import atnf.atoms.util.Angle;
 
 /**
  * Translation class using a mathematical expression.
- *
- * <P>The "init" argument must be an expression, where x is the current
- * monitor point value and the result of evaluating the expression produces
- * the translated point value.
- *
- * <P>Standard functions like log(), sqrt(), etc are understood.
- * Check out <code>http://www.singularsys.com/jep/doc/html/index.html</code>
- * for detailed documentation on JEP, the Java Mathematical Expression Parser.
- *
+ * 
+ * <P>
+ * The "init" argument must be an expression, where x is the current monitor
+ * point value and the result of evaluating the expression produces the
+ * translated point value.
+ * 
+ * <P>
+ * Standard functions like log(), sqrt(), etc are understood. Check out
+ * <code>http://www.singularsys.com/jep/doc/html/index.html</code> for
+ * detailed documentation on JEP, the Java Mathematical Expression Parser.
+ * 
  * @author Le Cuong Nguyen
  * @author David Brodrick
  * @version $Id: TranslationEQ.java,v 1.3 2005/08/15 01:55:17 bro764 Exp $
  */
-public class
-TranslationEQ
-extends Translation
+public class TranslationEQ extends Translation
 {
   /** Used for parsing and evaluating the expression. */
   JEP itsParser = new JEP();
 
-  protected static String itsArgs[] = new String[]{
-    "Translation using equation", "EQ",
-    "Equation, use X as independent variable", "java.lang.String"};
+  protected static String itsArgs[] = new String[] { "Translation using equation", "EQ", "Equation, use X as independent variable",
+      "java.lang.String" };
 
-   public TranslationEQ(PointDescription parent, String[] init)
-   {
-     super(parent, init);
+  public TranslationEQ(PointDescription parent, String[] init)
+  {
+    super(parent, init);
 
-     //precondition
-     if (init==null || init.length<1) {
-       System.err.println("TranslationEQ for " + itsParent.getLongName() +
-			  ": EXPECT EQUATION ARGUMENT!");
-     } else {
-       itsParser.setImplicitMul(true);
-       itsParser.setAllowUndeclared(true);
-       itsParser.addStandardConstants();
-       itsParser.addStandardFunctions();
-       itsParser.parseExpression(init[0]);
-     }
-   }
+    // precondition
+    if (init == null || init.length < 1) {
+      System.err.println("TranslationEQ for " + itsParent.getFullName() + ": EXPECT EQUATION ARGUMENT!");
+    } else {
+      itsParser.setImplicitMul(false);
+      itsParser.setAllowUndeclared(true);
+      itsParser.addStandardConstants();
+      itsParser.addStandardFunctions();
+      itsParser.parseExpression(init[0]);
+    }
+  }
 
-
-   public
-   PointData
-   translate(PointData data)
-   {
-     if (data==null) {
+  public PointData translate(PointData data)
+  {
+    if (data == null) {
       return null;
     }
-     if (data.getData()==null) {
-      //Return a null result
+
+    Object val = data.getData();
+    if (data.getData() == null) {
+      // Return a null result
       return new PointData(itsParent.getFullName());
-     }
+    }
 
-     Object dataCopy = data.getData();
-      if (!(dataCopy instanceof Number) && !(dataCopy instanceof Angle)) {
-	if (dataCopy instanceof Object[]) {
-    return new PointData(itsParent.getFullName(), 
-    						       data.getTimestamp(), translateArray((Object[])dataCopy));
-  } else {
-    return null;
-  }
-      }
-      // Really atrocious solution, but I believe its fast
-      if (dataCopy instanceof Angle) {
-          itsParser.addVariable("x",((Angle)data.getData()).getValue());
+    if (val instanceof Boolean) {
+      if (((Boolean) val).booleanValue()) {
+        itsParser.addVariable("x", 1.0);
       } else {
-	itsParser.addVariable("x",((Number)data.getData()).doubleValue());
+        itsParser.addVariable("x", 0.0);
       }
-      double resNum = itsParser.getValue();
+    } else if (val instanceof Angle) {
+      itsParser.addVariable("x", ((Angle) val).getValue());
+    } else {
+      itsParser.addVariable("x", val);
+    }
 
-      return new PointData(itsParent.getFullName(), data.getTimestamp(), new Double(resNum));
-   }
+    // Parse the expression using new value
+    Object res = itsParser.getValueAsObject();
 
+    // Check for parse error
+    if (itsParser.hasError()) {
+      System.err.println("TranslationEQ(" + itsParent.getFullName() + ": " + itsParser.getErrorInfo());
+    }
 
-   /** Translate an array. */
-   private
-   Object
-   translateArray(Object[] data)
-   {
-      Object[] resData = (Object[])Array.newInstance(data[0].getClass(), data.length);
-
-      for (int i = 0; i < data.length; i++) {
-         itsParser.addVariable("x", ((Number)data[i]).doubleValue());
-	 double res = itsParser.getValue();
-	 if (data[0] instanceof Byte) {
-    resData[i] = new Byte((byte)res);
-  } else if (data[0] instanceof Short) {
-    resData[i] = new Short((short)res);
-  } else if (data[0] instanceof Long) {
-    resData[i] = new Long((long)res);
-  } else if (data[0] instanceof Integer) {
-    resData[i] = new Integer((int)res);
-  } else if (data[0] instanceof Float) {
-    resData[i] = new Float((float)res);
-  } else {
-    resData[i] = new Double(res);
+    return new PointData(itsParent.getFullName(), data.getTimestamp(), res);
   }
-      }
-      return resData;
-   }
 
   public static String[] getArgs()
   {
-     return itsArgs;
+    return itsArgs;
   }
 }
