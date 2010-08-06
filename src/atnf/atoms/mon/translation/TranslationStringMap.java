17fdbded5deccc4050de7bdd8110b8d2391fa2ca
brodrick <brodrick@73e435cf-5d48-0410-ba38-3de2b9e16419>
2010-08-06 04:08:07 +0000

    Enable put/control in ASCIISocket. Add new StringTrim translation.

--- src/atnf/atoms/mon/translation/TranslationStringMap.java
+++ src/atnf/atoms/mon/translation/TranslationStringMap.java
@@ -17,77 +17,68 @@ import atnf.atoms.mon.PointDescription;
 /**
  * Map between input strings and an output strings. Init arguments must be in
  * form "input1:output1""input2:output2", eg "true:ok""false:DISCONNECTED".
- *
- * <P>Any unrecognised input are passed through without alteration.
- *
+ * 
+ * <P>
+ * Any unrecognised input are passed through without alteration.
+ * 
  * @author David Brodrick
- * @version $Id: TranslationStringMap.java,v 1.1 2004/11/01 04:46:46 bro764 Exp $
  */
-public class
-TranslationStringMap
-extends Translation
+public class TranslationStringMap extends Translation
 {
   /** Contains the mapping from inputs to outputs. */
-  protected HashMap<String,String> itsMappings = new HashMap<String,String>();
+  protected HashMap<String, String> itsMappings = new HashMap<String, String>();
 
-  protected static String[] itsArgs = new String[]{"Translation String Map",
-  "String Map"};
+  protected static String[] itsArgs = new String[] { "Translation String Map", "String Map" };
 
   public TranslationStringMap(PointDescription parent, String[] init)
   {
     super(parent, init);
 
-    for (int i=0; i<init.length; i++) {
-      //Check for the :
+    for (int i = 0; i < init.length; i++) {
+      // Check for the :
       int colonindex = init[i].indexOf(":");
-      if (colonindex==-1) {
-	System.err.println("TranslationStringMap: \"" + parent.getName() +
-			   "\" Expected \"input_string:output_string\"");
-	continue;
+      if (colonindex == -1) {
+        System.err.println("TranslationStringMap: \"" + parent.getName() + "\" Expected \"input_string:output_string\"");
+        continue;
       }
       String inp = init[i].substring(0, colonindex);
-      String out = init[i].substring(colonindex+1);
-      //Save the mapping for later use
+      String out = init[i].substring(colonindex + 1);
+      // Save the mapping for later use
       itsMappings.put(inp, out);
     }
   }
 
-
   /** Map the input data to an output string. */
-  public
-  PointData
-  translate(PointData data)
+  public PointData translate(PointData data)
   {
-    //preconditions
-    if (data==null) {
+    // preconditions
+    if (data == null) {
       return null;
     }
     Object val = data.getData();
 
-    //If we got null-data then throw a null-data result
-    if (val==null) {
+    // If we got null-data then throw a null-data result
+    if (val == null) {
       return new PointData(itsParent.getFullName());
     }
 
-    //Get value as a string
+    // Get value as a string
     String strval = val.toString();
-    //Do a lookup on the string value
-    String newstr = (String)itsMappings.get(strval);
+    // Do a lookup on the string value
+    String newstr = (String) itsMappings.get(strval);
 
-    if (newstr==null) {
+    if (newstr == null) {
       newstr = strval;
     }
 
-    //Create return structure with right details
-    PointData res = new PointData(itsParent.getFullName(), 
-                                  data.getTimestamp(), newstr);
+    // Create return structure with right details
+    PointData res = new PointData(itsParent.getFullName(), data.getTimestamp(), newstr);
 
     return res;
   }
 
-
   public static String[] getArgs()
   {
-     return itsArgs;
+    return itsArgs;
   }
 }
