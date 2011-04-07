0134cdba3f71b5c11eb3a6521abc1fda333fca51
brodrick@gmail.com <brodrick@gmail.com@73e435cf-5d48-0410-ba38-3de2b9e16419>
2011-04-07 03:57:45 +0000

    Add some new Translations for timing.

---
+++ src/atnf/atoms/mon/translation/TranslationSinceHighTimer.java
@@ -0,0 +1,75 @@
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+
+package atnf.atoms.mon.translation;
+
+import org.apache.log4j.Logger;
+
+import atnf.atoms.time.*;
+import atnf.atoms.mon.*;
+import atnf.atoms.mon.translation.Translation;
+
+/**
+ * Measures the amount of time since the input was last in a high/mark state.
+ * This requires the input to be a Boolean or a Number. The output is a RelTime
+ * type. The output will not be able to be produced until the input has gone
+ * high for the first time.
+ * 
+ * @author David Brodrick
+ */
+public class TranslationSinceHighTimer extends Translation {
+  /** The time the input was last high. */
+  protected AbsTime itsLastHigh;
+
+  /** Constructor. */
+  public TranslationSinceHighTimer(PointDescription parent, String[] init) {
+    super(parent, init);
+  }
+
+  /** Determine output based on input. */
+  public PointData translate(PointData data) {
+    RelTime output = null;
+    AbsTime now = data.getTimestamp();
+
+    // Get the input as a Boolean
+    Object rawinput = data.getData();
+    Boolean inputstate = null;
+    if (rawinput instanceof Boolean) {
+      inputstate = (Boolean) rawinput;
+    } else if (rawinput instanceof Number) {
+      int intval = ((Number) rawinput).intValue();
+      if (intval == 0) {
+        inputstate = new Boolean(false);
+      } else {
+        inputstate = new Boolean(true);
+      }
+    }
+
+    if (inputstate != null) {
+      if (inputstate.booleanValue()) {
+        // Input is currently high, so no time has elapsed
+        itsLastHigh = now;
+        output = RelTime.factory(0);
+      } else if (itsLastHigh != null) {
+        // Calculate the time since the input was last high
+        output = Time.diff(now, itsLastHigh);
+      } else {
+        // Unable to calculate output value as output has always been low
+        output = null;
+      }
+    } else {
+      Logger logger = Logger.getLogger(this.getClass().getName());
+      logger.error("(" + itsParent.getFullName() + "): Expect Boolean or Number input");
+      output = null;
+    }
+    if (output != null) {
+      return new PointData(itsParent.getFullName(), now, output);
+    } else {
+      return null;
+    }
+  }
+}
