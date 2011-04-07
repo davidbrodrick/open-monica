a4dc04e89554f1e8a934042baa219d5e270c7404
brodrick@gmail.com <brodrick@gmail.com@73e435cf-5d48-0410-ba38-3de2b9e16419>
2011-04-07 23:42:30 +0000

    Formatting.

--- src/atnf/atoms/mon/translation/TranslationEnumMap.java
+++ src/atnf/atoms/mon/translation/TranslationEnumMap.java
@@ -14,14 +14,12 @@ import atnf.atoms.mon.PointDescription;
 
 /**
  * any argument with out a : is considered to define the default string
- *
+ * 
  * @author David Brodrick
- * @version $Id: TranslationEnumMap.java,v 1.3 2004/10/28 23:44:58 bro764 Exp bro764 $
+ * @version $Id: TranslationEnumMap.java,v 1.3 2004/10/28 23:44:58 bro764 Exp
+ *          bro764 $
  */
-public class
-TranslationEnumMap
-extends Translation
-{
+public class TranslationEnumMap extends Translation {
   /** Array of integer enumeration values. */
   protected int[] itsValues = null;
 
@@ -31,91 +29,81 @@ extends Translation
   /** String to use if no enum values match. */
   protected String itsDefault = "INVALID";
 
-  protected static String[] itsArgs = new String[]{"Translation Enum Map",
-  "Enum Map"};
+  protected static String[] itsArgs = new String[] { "Translation Enum Map", "Enum Map" };
 
-  public TranslationEnumMap(PointDescription parent, String[] init)
-  {
+  public TranslationEnumMap(PointDescription parent, String[] init) {
     super(parent, init);
 
-    //Count how many mapping arguments there are
+    // Count how many mapping arguments there are
     int count = 0;
-    for (int i=0; i<init.length; i++) {
-      if (init[i].indexOf(":")!=-1) {
+    for (int i = 0; i < init.length; i++) {
+      if (init[i].indexOf(":") != -1) {
         count++;
       }
     }
 
-    //Allocate containers of the correct size
+    // Allocate containers of the correct size
     itsValues = new int[count];
     itsMappings = new String[count];
 
-    //Parse and record values
+    // Parse and record values
     count = 0;
-    for (int i=0; i<init.length; i++) {
+    for (int i = 0; i < init.length; i++) {
       int sep = init[i].indexOf(":");
-      if (sep==-1) {
-	//Must be the "default" string value
+      if (sep == -1) {
+        // Must be the "default" string value
         itsDefault = init[i];
       } else {
-	//Break string into value:mapping parts
-	String val = init[i].substring(0, sep).trim();
-	String map = init[i].substring(sep+1).trim();
-	itsValues[count] = Integer.parseInt(val);
+        // Break string into value:mapping parts
+        String val = init[i].substring(0, sep).trim();
+        String map = init[i].substring(sep + 1).trim();
+        itsValues[count] = Integer.parseInt(val);
         itsMappings[count] = map;
-	count++;
+        count++;
       }
     }
   }
 
-
   /** Calculate the delta and return new value. */
-  public
-  PointData
-  translate(PointData data)
-  {
-    //preconditions
-    if (data==null) {
+  public PointData translate(PointData data) {
+    // preconditions
+    if (data == null) {
       return null;
     }
     Object val = data.getData();
 
-    //If we got null-data then throw a null-data result
-    if (val==null) {
+    // If we got null-data then throw a null-data result
+    if (val == null) {
       return new PointData(itsParent.getFullName());
     }
 
     if (!(val instanceof Number)) {
-      //Probably an error somewhere so print a message
-      System.err.println("TranslationEnumMap for " + itsParent.getSource()
-			 + "." + itsParent.getName() + ": Got NON-NUMERIC"
-			 + " data..");
+      // Probably an error somewhere so print a message
+      System.err.println("TranslationEnumMap for " + itsParent.getSource() + "." + itsParent.getName() + ": Got NON-NUMERIC" + " data..");
       return null;
     }
 
-    //Get the value
-    int d = ((Number)val).intValue();
-    //See if it corresponds to a value in our mapping set
+    // Get the value
+    int d = ((Number) val).intValue();
+    // See if it corresponds to a value in our mapping set
     String m = null;
-    for (int i=0; i<itsValues.length; i++) {
-      if (itsValues[i]==d) {
+    for (int i = 0; i < itsValues.length; i++) {
+      if (itsValues[i] == d) {
         m = itsMappings[i];
       }
     }
-    if (m==null) {
-      //Didn't find a match, so use our default value
+    if (m == null) {
+      // Didn't find a match, so use our default value
       m = itsDefault;
     }
 
-    //Create return structure with right details
+    // Create return structure with right details
     PointData res = new PointData(itsParent.getFullName(), m);
 
     return res;
   }
 
-
-  public static String[] getArgs()
-  {
-     return itsArgs;
+  public static String[] getArgs() {
+    return itsArgs;
   }
 }
