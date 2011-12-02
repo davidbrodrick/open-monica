fc98b6b58520e64df1b68ca2dadc95715dc0905d
camnic3@gmail.com <camnic3@gmail.com@73e435cf-5d48-0410-ba38-3de2b9e16419>
2011-12-02 04:12:47 +0000

    Updated AlarmCheck methods to allow for alarms to be set after a specified occurrence of update values.

--- trunk/src/atnf/atoms/mon/alarmcheck/AlarmCheckRange.java
+++ trunk/src/atnf/atoms/mon/alarmcheck/AlarmCheckRange.java
@@ -8,98 +8,135 @@
 
 package atnf.atoms.mon.alarmcheck;
 
+import org.apache.log4j.Logger;
+
 import atnf.atoms.mon.*;
 import atnf.atoms.util.Angle;
 
 /**
- * <i>AlarmCheck</i> sub-class which checks if a numeric data value is within a nominated
- * range. The first two <i>init</i> arguments should be the lower and upper limits of the
- * range. An optional third argument defines whether inside the range is considered
- * nominal (<tt>True</tt> - the default) or outside the range is nominal (<tt>False</tt>).
+ * <i>AlarmCheck</i> sub-class which checks if a numeric data value is within a
+ * nominated range. The first two <i>init</i> arguments should be the lower and
+ * upper limits of the range. An optional third argument defines whether inside
+ * the range is considered nominal (<tt>True</tt> - the default) or outside the
+ * range is nominal (<tt>False</tt>).
  * 
  * @author David Brodrick
  * @author Le Cuong Nguyen
  */
-public class AlarmCheckRange extends AlarmCheck
-{
-  /** The lower limit of the range. */
-  private double itsLower = 0.0;
+public class AlarmCheckRange extends AlarmCheck {
+	/** The lower limit of the range. */
+	private double itsLower = 0.0;
 
-  /** The upper limit of the range. */
-  private double itsUpper = 0.0;
+	/** The upper limit of the range. */
+	private double itsUpper = 0.0;
 
-  /**
-   * Is it normal to be inside the range (<tt>true</tt>) or is a normal value outide
-   * the range (<tt>false</tt>).
-   */
-  private boolean itsInsideNormal = true;
+	/**
+	 * Is it normal to be inside the range (<tt>true</tt>) or is a normal value
+	 * outside the range (<tt>false</tt>).
+	 */
+	private boolean itsInsideNormal = true;
 
-  public AlarmCheckRange(PointDescription parent, String[] args)
-  {
-    super(parent, args);
+	/**
+	 * Minimum amount of times a value must appear in a row before raising an
+	 * alarm
+	 */
+	private int itsUpdateAmt = 0;
 
-    if (args.length < 2) {
-      // This is a problem
-      System.err.println("AlarmCheckRange: Need at least two arguments!!!");
-      itsUpper = itsLower = 0.0;
-    } else {
-      // Read the upper and lower limits of the numeric range
-      itsLower = Double.parseDouble(args[0]);
-      itsUpper = Double.parseDouble(args[1]);
-      if (itsLower > itsUpper) {
-        // Need to swap!
-        double temp = itsUpper;
-        itsUpper = itsLower;
-        itsLower = temp;
-      }
-      // Check if the optional third argument was specified
-      if (args.length == 3) {
-        if (args[2].equalsIgnoreCase("true") || args[2].equalsIgnoreCase("t") || args[2].equalsIgnoreCase("1")) {
-          itsInsideNormal = true;
-        } else {
-          itsInsideNormal = false;
-        }
-      }
-    }
-  }
+	/**
+	 * The amount of times an alarm value has appeared in a row
+	 */
+	private int itsBoolCount = 0;
 
-  /**
-   * Checks if the value is "normal" with respect to the specified limits.
-   * @param data The value to check against our limits.
-   */
-  public void checkAlarm(PointData data)
-  {
-    // All zeroes means that we shouldn't even check
-    if (itsUpper == itsLower && itsLower == 0.0) {
-      return;
-    }
-    // No news is good news, right?
-    if (data == null || data.getData() == null) {
-      return;
-    }
+	public AlarmCheckRange(PointDescription parent, String[] args)
+			throws IllegalArgumentException {
+		super(parent, args);
 
-    Object myData = data.getData();
-    double doubleData = 0.0;
-    // Get the data as a double
-    if (myData instanceof Number) {
-      doubleData = ((Number) myData).doubleValue();
-    } else if (myData instanceof Angle) {
-      doubleData = ((Angle) myData).getValue();
-    } else {
-      System.err.println("AlarmCheckRange: ERROR: " + data.getName() + " has NON-NUMERIC data!");
-      return;
-    }
+		try {
+			// Read the upper and lower limits of the numeric range
+			itsLower = Double.parseDouble(args[0]);
+			itsUpper = Double.parseDouble(args[1]);
 
-    // Check if the number is within the specified range
-    boolean withinRange = false;
-    if (doubleData >= itsLower && doubleData <= itsUpper) {
-      withinRange = true;
-    }
+			if (itsLower > itsUpper) {
+				// Need to swap!
+				double temp = itsUpper;
+				itsUpper = itsLower;
+				itsLower = temp;
+			}
+			// Check if the optional third argument was specified
+			// Store the boolean argument for use in the alarm checking.
+			if (args.length > 2) {
+				itsInsideNormal = Boolean.parseBoolean(args[2]);
+			}
 
-    // If it's inside the range and it's supposed to be, then all is well
-    if ((!itsInsideNormal && withinRange) || (itsInsideNormal && !withinRange)) {
-      data.setAlarm(true);
-    }
-  }
+			// This is the number of times a value should appear before setting
+			// the alarm to True.
+			// If no argument has been specified this defaults to an update
+			// amount of 1.
+			if (args.length > 3) {
+				itsUpdateAmt = Integer.parseInt(args[3]);
 
+			} else {
+				itsUpdateAmt = 1;
+			}
+
+		} catch (Exception e) {
+			Logger logger = Logger.getLogger(Factory.class.getName());
+			logger.error("AlarmCheckRange: Need at least two arguments!");
+
+			itsUpper = itsLower = 0.0;
+		}
+	}
+
+	/**
+	 * Checks if the value is "normal" with respect to the specified limits.
+	 * 
+	 * @param data
+	 *            The value to check against our limits.
+	 */
+	public void checkAlarm(PointData data) {
+		// All zeroes means that we shouldn't even check
+		if (itsUpper == itsLower && itsLower == 0.0) {
+			return;
+		}
+		// No news is good news, right?
+		if (data == null || data.getData() == null) {
+			return;
+		}
+
+		Object myData = data.getData();
+		double doubleData = 0.0;
+
+		// Retrieve the data as a double
+		if (myData instanceof Number) {
+			doubleData = ((Number) myData).doubleValue();
+		} else if (myData instanceof Angle) {
+			doubleData = ((Angle) myData).getValue();
+		} else {
+			Logger logger = Logger.getLogger(Factory.class.getName());
+			logger.error("AlarmCheckRange: ERROR: " + data.getName()
+					+ " has NON-NUMERIC data!");
+			return;
+		}
+
+		// Check if the number is within the specified range
+		boolean withinRange = false;
+		if (doubleData >= itsLower && doubleData <= itsUpper) {
+			withinRange = true;
+		}
+
+		// If it's inside the range and it's supposed to be, then all is well
+		if ((!itsInsideNormal && withinRange)
+				|| (itsInsideNormal && !withinRange)) {
+			itsBoolCount++;
+		} else {
+			itsBoolCount = 0;
+		}
+
+		// If the alarm count is the same as the amount specified in the config
+		// file, set
+		// the alarm to True.
+		if (itsBoolCount >= itsUpdateAmt) {
+			data.setAlarm(true);
+		}
+	}
 }
--- trunk/src/atnf/atoms/mon/alarmcheck/AlarmCheckStringMatch.java
+++ trunk/src/atnf/atoms/mon/alarmcheck/AlarmCheckStringMatch.java
@@ -8,17 +8,19 @@
 
 package atnf.atoms.mon.alarmcheck;
 
+import org.apache.log4j.Logger;
+
 import atnf.atoms.mon.*;
 
 /**
- * <i>AlarmCheck</i> sub-class which checks if a String data-value matches a predefined
- * set. We can work in one of two ways, either considering the new value to be <i>okay</i>
- * if it matches one of the strings in the set, or considering it to be <i>in error</i>
- * if it matches a string in the set.
+ * <i>AlarmCheck</i> sub-class which checks if a String data-value matches a
+ * predefined set. We can work in one of two ways, either considering the new
+ * value to be <i>okay</i> if it matches one of the strings in the set, or
+ * considering it to be <i>in error</i> if it matches a string in the set.
  * <P>
- * The first argument determines which of these behaviors we use. If the argument is
- * <tt>true</tt> then we consider a string match be be good. If the first argument is
- * false then we consider a string match to be bad.
+ * The first argument determines which of these behaviors we use. If the
+ * argument is <tt>true</tt> then we consider a string match be be good. If the
+ * first argument is false then we consider a string match to be bad.
  * <P>
  * All the remaining arguments must be the strings to check against.
  * <P>
@@ -26,62 +28,90 @@ import atnf.atoms.mon.*;
  * 
  * @author David Brodrick
  */
-public class AlarmCheckStringMatch extends AlarmCheck
-{
-  /** Does a match indicate ok (true) or error (false). */
-  protected boolean itsMatchGood = true;
+public class AlarmCheckStringMatch extends AlarmCheck {
+	/** Does a match indicate OK (true) or error (false). */
+	protected boolean itsMatchGood = true;
 
-  /** Strings to check the data values against. */
-  protected String[] itsStringSet = null;
+	/** Strings to check the data values against. */
+	protected String[] itsStringSet = null;
+	
+	/**
+	 * Minimum amount of times a value must appear in a row before raising an
+	 * alarm
+	 */
+	private int itsUpdateAmt = 0;
+	
+	/**
+	 * The amount of times an alarm value has appeared in a row
+	 */
+	private int itsBoolCount = 0;
 
-  public AlarmCheckStringMatch(PointDescription parent, String[] args)
-  {
-    super(parent, args);
+	public AlarmCheckStringMatch(PointDescription parent, String[] args)
+			throws IllegalArgumentException, NumberFormatException {
+		super(parent, args);
+		
+		int argval = 1;
+		
+		try {
+			boolean firstArgInteger = true;
+			// If the first argument provided is an Integer value then user has specified minimum number of updates before raising an alarm.
+			try {
+				itsUpdateAmt = Integer.parseInt(args[0]);
+			} catch (NumberFormatException n) {
+				// First argument is not an integer hence the update amount has not been specified.
+				firstArgInteger = false;
+				itsUpdateAmt = 1; // Set default update amount to 1.
+			}
+			if (!firstArgInteger) { // If boolean is first argument
+				// First check which mode to run in
+				itsMatchGood = Boolean.parseBoolean(args[0]);
+				argval = 1;
+			} else { // If Integer value is first argument
+				itsMatchGood = Boolean.parseBoolean(args[1]);
+				argval = 2;
+			}
+			
+			// The next arguments are strings to check against
+			itsStringSet = new String[args.length - argval];
+			for (int i=argval; i < args.length; i++) {
+				itsStringSet[i - argval] = args[i].trim();
+			}
+		} catch (Exception e) {
+			Logger logger = Logger.getLogger(Factory.class.getName());
+			logger.error("AlarmCheckStringMatch: Not enough arguments given!");
+		}
+	}
 
-    if (args.length < 2) {
-      // This is a problem
-      System.err.println("AlarmCheckStringMatch: Need at least two arguments!!!");
-    } else {
-      // First check which mode to run in
-      if (args[0].equalsIgnoreCase("true") || args[0].equalsIgnoreCase("t") || args[0].equalsIgnoreCase("1")) {
-        itsMatchGood = true;
-      } else {
-        itsMatchGood = false;
-      }
-      // The rest of the arguments are strings to check against
-      itsStringSet = new String[args.length - 1];
-      for (int i = 1; i < args.length; i++) {
-        itsStringSet[i - 1] = args[i].trim();
-      }
-    }
-  }
+	public void checkAlarm(PointData data) {
+		// No strings means we shouldn't even bother
+		if (itsStringSet == null) {
+			return;
+		}
+		// No news is good news, right?
+		if (data == null || data.getData() == null) {
+			return;
+		}
 
-  public void checkAlarm(PointData data)
-  {
-    // No strings means we shouldn't even bother
-    if (itsStringSet == null) {
-      return;
-    }
-    // No news is good news, right?
-    if (data == null || data.getData() == null) {
-      return;
-    }
+		// Get the data in string form
+		String strData = data.getData().toString().trim();
 
-    // Get the data in string form
-    String strData = data.getData().toString().trim();
+		// Check the string against each string in our set
+		boolean havematch = false;
+		for (int i = 0; i < itsStringSet.length; i++) {
+			if (strData.equalsIgnoreCase(itsStringSet[i])) {
+				havematch = true;
+				break;
+			}
+		}
 
-    // Check the string against each string in our set
-    boolean havematch = false;
-    for (int i = 0; i < itsStringSet.length; i++) {
-      if (strData.equalsIgnoreCase(itsStringSet[i])) {
-        havematch = true;
-        break;
-      }
-    }
-
-    if ((!havematch && itsMatchGood) || (havematch && !itsMatchGood)) {
-      // Found an alarm condition
-      data.setAlarm(true);
-    }
-  }
+		if ((!havematch && itsMatchGood) || (havematch && !itsMatchGood)) {
+			itsBoolCount++;
+		} else {
+			itsBoolCount = 0;
+		}
+		
+		if (itsBoolCount >= itsUpdateAmt) {
+			data.setAlarm(true);
+		}
+	}
 }
