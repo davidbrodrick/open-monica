4f3f1df3569319c58ab81ddc2a9e429c571356b5
brodrick@gmail.com <brodrick@gmail.com@73e435cf-5d48-0410-ba38-3de2b9e16419>
2012-02-08 22:32:57 +0000

    Update doc's.

--- src/atnf/atoms/mon/translation/TranslationCalculation.java
+++ src/atnf/atoms/mon/translation/TranslationCalculation.java
@@ -15,31 +15,29 @@ import atnf.atoms.mon.PointDescription;
 import atnf.atoms.util.Angle;
 
 /**
- * Calculate an output value which is a function one of one or more listened-to
- * points. Expression parsing is performed using JEP, the Java Expression
- * Parser, which makes this Translation highly flexible and very useful.
+ * Calculate an output value which is a function one of one or more listened-to points. Expression parsing is performed using JEP,
+ * the Java Expression Parser, which makes this Translation highly flexible and very useful. One difference from the JEP
+ * documentation is that strings in the expression must be quoted in single quotes not double quotes.
+ * 
  * <P>
- * The first constructor <i>init</i> argument needs to specify the number, N,
- * of monitor points which will be listened to. The next N arguments must be the
- * names of the points being listened to. The final argument is the expression
- * to be evaluated where each input variable is named a, b, c, etc,
- * corresponding to the order they were named.
+ * The first constructor <i>init</i> argument needs to specify the number, N, of monitor points which will be listened to. The next
+ * N arguments must be the names of the points being listened to. The final argument is the expression to be evaluated where each
+ * input variable is named a, b, c, etc, corresponding to the order they were named.
+ * 
  * <P>
- * This implementation clears all inputs after a value is calculated, so that a
- * subsequent calculation will not happen until all inputs have updated again.
+ * This implementation clears all inputs after a value is calculated, so that a subsequent calculation will not happen until all
+ * inputs have updated again.
  * 
  * @author David Brodrick
  */
-public class TranslationCalculation extends TranslationListener
-{
-  protected static String[] itsArgs = new String[] { "Calculation", "Listens to two other points", "NumPoints", "Integer",
-      "MonitorPoint 1", "String", "MonitorPoint N", "String", "Function", "String" };
+public class TranslationCalculation extends TranslationListener {
+  protected static String[] itsArgs = new String[] { "Calculation", "Listens to two other points", "NumPoints", "Integer", "MonitorPoint 1", "String",
+      "MonitorPoint N", "String", "Function", "String" };
 
   /** Used for parsing and evaluating the expression. */
   JEP itsParser = new JEP();
 
-  public TranslationCalculation(PointDescription parent, String[] init)
-  {
+  public TranslationCalculation(PointDescription parent, String[] init) {
     super(parent, init);
 
     if (init.length < itsNumPoints + 2) {
@@ -55,11 +53,10 @@ public class TranslationCalculation extends TranslationListener
   }
 
   /**
-   * Provide the current input values to the expression parser, calculate result
-   * and clear all input variables so that fresh values will be used next time.
+   * Provide the current input values to the expression parser, calculate result and clear all input variables so that fresh values
+   * will be used next time.
    */
-  protected Object doCalculations()
-  {
+  protected Object doCalculations() {
     for (int i = 0; i < itsNumPoints; i++) {
       // Update the value for this variable
       String thisvar = "" + ((char) (('a') + i));
