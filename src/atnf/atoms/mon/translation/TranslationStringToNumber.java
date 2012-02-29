62498ccd00474fb77cba2f6b617acb88089b1122
brodrick@gmail.com <brodrick@gmail.com@73e435cf-5d48-0410-ba38-3de2b9e16419>
2012-02-29 04:10:51 +0000

    Add support for Long data types.

--- src/atnf/atoms/mon/translation/TranslationStringToNumber.java
+++ src/atnf/atoms/mon/translation/TranslationStringToNumber.java
@@ -8,13 +8,14 @@
 
 package atnf.atoms.mon.translation;
 
+import org.apache.log4j.Logger;
+
 import atnf.atoms.mon.PointData;
 import atnf.atoms.mon.PointDescription;
 
 /**
- * This class translates a String data value to a sub-class of Number.
- * Currently, only Float, Double and Integer are supported. An optional radix
- * argument may be used for Integers. If radix is omitted, base 10 is assumed.
+ * This class translates a String data value to a sub-class of Number. Float, Double, Integer and Long are supported. An optional
+ * radix argument may be used for Integers. If radix is omitted, base 10 is assumed.
  * 
  * @author Simon Hoyle
  * @author David Brodrick
@@ -23,8 +24,11 @@ import atnf.atoms.mon.PointDescription;
 
 public class TranslationStringToNumber extends Translation {
   /** Arguments. Only the first argument is essential. */
-  protected static String itsArgs[] = new String[] { "Translation StringToNumber", "StringToNumber", "NumericTypeName",
-      "java.lang.String", "Radix", "java.lang.String" };
+  protected static String itsArgs[] = new String[] { "Translation StringToNumber", "StringToNumber", "NumericTypeName", "java.lang.String", "Radix",
+      "java.lang.String" };
+
+  /** Logger */
+  protected static Logger theirLogger = Logger.getLogger(TranslationStringToNumber.class.getName());
 
   /**  */
   protected String itsNumericTypeName = null;
@@ -39,8 +43,7 @@ public class TranslationStringToNumber extends Translation {
       if (init.length > 1)
         itsRadix = Integer.parseInt(init[1]);
     } else
-      System.err.println("ERROR: TranslationStringToNumber (for " + itsParent.getName()
-          + "): Expect 1 or 2 Arguments.. got " + init.length);
+      System.err.println("ERROR: TranslationStringToNumber (for " + itsParent.getName() + "): Expect 1 or 2 Arguments.. got " + init.length);
   }
 
   /** Perform the actual data translation. */
@@ -59,21 +62,25 @@ public class TranslationStringToNumber extends Translation {
     } else {
       d = d.toString().trim();
     }
-    
+
     try {
-      if (itsNumericTypeName.equalsIgnoreCase("Float"))
+      if (itsNumericTypeName.equalsIgnoreCase("Float")) {
         num = Float.valueOf((String) d);
-      else if (itsNumericTypeName.equalsIgnoreCase("Double"))
+      } else if (itsNumericTypeName.equalsIgnoreCase("Double")) {
         num = Double.valueOf((String) d);
-      else if (itsNumericTypeName.equalsIgnoreCase("Integer")) {
+      } else if (itsNumericTypeName.equalsIgnoreCase("Integer")) {
         if (itsRadix != 0)
           num = Integer.valueOf((String) d, itsRadix);
         else
           num = Integer.valueOf((String) d);
+      } else if (itsNumericTypeName.equalsIgnoreCase("Long")) {
+        if (itsRadix != 0)
+          num = Long.valueOf((String) d, itsRadix);
+        else
+          num = Long.valueOf((String) d);
       }
     } catch (NumberFormatException e) {
-      // System.err.println("ERROR: TranslationStringToNumber: (for "
-      // + data.getName() + "): " + e.toString());
+      theirLogger.error(itsParent.getFullName() + ": " + e);
     }
 
     res = new PointData(data.getName(), data.getTimestamp(), num);
