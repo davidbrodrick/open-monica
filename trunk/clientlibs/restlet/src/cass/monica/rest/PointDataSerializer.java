03f97b1d223b7b9342b8150a967f7bc6d762c111
brodrick@gmail.com <brodrick@gmail.com@73e435cf-5d48-0410-ba38-3de2b9e16419>
2012-04-17 23:52:34 +0000

    Add custom serializer for PointData.

---
+++ trunk/clientlibs/restlet/src/cass/monica/rest/PointDataSerializer.java
@@ -0,0 +1,42 @@
+/*
+ *  Copyright (c) 2012 CSIRO Astronomy and Space Science (CASS), Commonwealth
+ * Scientific and Industrial Research Organisation (CSIRO) PO Box 76, Epping NSW 1710,
+ * Australia atnf-enquiries@csiro.au
+ * 
+ * MoniCA is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either 
+ * version 2 of the License, or (at your option) any later version.
+ */
+
+package cass.monica.rest;
+
+import java.lang.reflect.Type;
+
+import atnf.atoms.mon.PointData;
+import atnf.atoms.time.AbsTime;
+
+import com.google.gson.*;
+
+/**
+ * Handle serialisation/deserialisation of PointDescription objects to JSON.
+ * 
+ * @author David Brodrick
+ */
+public class PointDataSerializer implements JsonSerializer<PointData> {
+  /** Serialise from a PointDescription to JSON, in the appropriate format. */
+  public JsonElement serialize(PointData src, Type typeOfSrc, JsonSerializationContext context) {
+    JsonObject res = new JsonObject();
+    if (src.getName() != null) {
+      res.addProperty("name", src.getName());
+    }
+    // How to use the AbsTime serialiser class from here?
+    res.addProperty("ts", src.getTimestamp().toString(AbsTime.Format.UTC_STRING));
+    if (src.getData() != null) {
+      res.addProperty("value", src.getData().toString());
+    }
+    if (src.getAlarm()) {
+      res.addProperty("alarm", src.getAlarm());
+    }
+    return res;
+  }
+}
