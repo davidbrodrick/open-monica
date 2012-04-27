00e9d21bb500e19d56fd05b46fc72d0baccdb029
Chris.Phillips@csiro.au <Chris.Phillips@csiro.au@73e435cf-5d48-0410-ba38-3de2b9e16419>
2012-04-27 00:12:02 +0000

    Allow setting of observatory

--- trunk/clientlibs/perl/examples/setvsic.pl
+++ trunk/clientlibs/perl/examples/setvsic.pl
@@ -4,15 +4,48 @@ use strict;
 
 use Astro::Time;
 use ATNF::MoniCA;
+use Getopt::Long;
 
-my $server = 'monhost-nar';
+my $obs;
+$obs = $ENV{VSICOBS} if (defined $ENV{VSICOBS});
+
+my $atca = 0;
+my $parkes = 0;
+my $mopra = 0;
+
+GetOptions('atca'=>\$atca, 'mopra'=>\$mopra, 'parkes'=>\$parkes);
+
+if ($atca) {
+  die "Specific just one observatory" if ($parkes || $mopra);
+  $obs = 'ATCA';
+} elsif ($mopra) {
+ die "Specific just one observatory" if ($parkes);
+  $obs = 'MOPRA';
+} elsif ($parkes) {
+  $obs = 'PARKES';
+}
+
+die "No observatory defined\n" if (!defined $obs);
+
+$obs = uc($obs);
+
+my $server;
+if ($obs eq 'ATCA') {
+  $server = 'monhost-nar';
+} elsif ($obs eq 'MOPRA') {
+  $server = 'monhost-mop';
+} elsif ($obs eq 'PARKES') {
+  $server = 'monhost-pks';
+} else {
+  die "Unrecognised observatory $obs\n";
+}
 
 die "Usage: setvsic.pl <MODE>\n" if (@ARGV != 1);
 
 my $code = hex(shift @ARGV);
 
 my $mon = monconnect($server);
-die "Could not connect to monitor host \"$server\"\n" if (!defined $mon);
+die "Could not connect to $obs MoniCA host \"$server\"\n" if (!defined $mon);
 
 my @setpoints;
 
@@ -22,6 +55,5 @@ push @setpoints, MonSetPoint({point => 'site.vlbi.vsic.control.Code',
 
 
 my $ret = monset($mon, 'a', 'b', @setpoints);
-
 
 print $ret->val, "\n";
