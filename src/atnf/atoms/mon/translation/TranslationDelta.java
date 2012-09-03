5a392e2f51917c5627de1f75081bc079c36d9ed9
brodrick@gmail.com <brodrick@gmail.com@73e435cf-5d48-0410-ba38-3de2b9e16419>
2012-09-03 04:29:55 +0000

    Return null rather than null-valued data.

--- src/atnf/atoms/mon/translation/TranslationDelta.java
+++ src/atnf/atoms/mon/translation/TranslationDelta.java
@@ -14,79 +14,74 @@ import atnf.atoms.mon.PointDescription;
 import atnf.atoms.util.Angle;
 
 /**
- * Calculate the difference between successive monitor point values, calculated as
- * <tt>prev_val - new_val</tt>. This class can handle <i>Number</i> or <i>Angle</i>
- * data types. Angles are treated specially in that we try to handle phase wraps
- * correctly, ie, all jumps are mapped to the range -180<=d<=180.
+ * Calculate the difference between successive monitor point values, calculated as <tt>prev_val - new_val</tt>. This class can
+ * handle <i>Number</i> or <i>Angle</i> data types. Angles are treated specially in that we try to handle phase wraps correctly, ie,
+ * all jumps are mapped to the range -180<=d<=180.
  * <P>
  * The output is always a Double data type.
  * 
  * @author David Brodrick
  */
-public class TranslationDelta extends Translation
-{
-    /** Previous value of the monitor point. */
-    Double itsPreviousValue = null;
+public class TranslationDelta extends Translation {
+  /** Previous value of the monitor point. */
+  Double itsPreviousValue = null;
 
-    protected static String[] itsArgs = new String[] { "Translation Delta", "Delta" };
+  protected static String[] itsArgs = new String[] { "Translation Delta", "Delta" };
 
-    public TranslationDelta(PointDescription parent, String[] init)
-    {
-        super(parent, init);
+  public TranslationDelta(PointDescription parent, String[] init) {
+    super(parent, init);
+  }
+
+  /** Calculate the delta and return new value. */
+  public PointData translate(PointData data) {
+    Object val = data.getData();
+    if (val == null) {
+      // There is no data, can't calculate a delta for this or next point
+      itsPreviousValue = null;
+      // Return null data to terminate translation for this point
+      return null;
     }
 
-    /** Calculate the delta and return new value. */
-    public PointData translate(PointData data)
-    {
-        Object val = data.getData();
-        if (val == null) {
-            // There is no data, can't calculate a delta for this or next point
-            itsPreviousValue = null;
-            // Return point with a null data field
-            return new PointData(itsParent.getFullName());
-        }
-
-        // Get the new value as a double
-        double dval;
-        if (val instanceof Number) {
-            dval = ((Number) val).doubleValue();
-        } else if (val instanceof Angle) {
-            dval = ((Angle) val).getValue();
-        } else {
-            System.err.println("TranslationDelta: " + itsParent.getFullName() + ": Got NON-NUMERIC data!");
-            return new PointData(itsParent.getFullName());
-        }
-
-        PointData res = null;
-
-        // If we had a previous value we can calculate the change
-        if (itsPreviousValue != null) {
-            // Calculate the change in the value of the point
-            double delta = itsPreviousValue.doubleValue() - dval;
-            if (val instanceof Angle) {
-                // Watch for phase wrap
-                if (delta > Math.PI) {
-                    delta = 2 * Math.PI - delta;
-                } else if (delta < -Math.PI) {
-                    delta = 2 * Math.PI + delta;
-                }
-                res = new PointData(itsParent.getFullName(), Angle.factory(delta, Angle.Format.RADIANS));
-            } else {
-                res = new PointData(itsParent.getFullName(), new Double(delta));
-            }
-        } else {
-            // No previous value so can't calculate delta, so return null data
-            res = new PointData(itsParent.getFullName());
-        }
-
-        // Record the current value for use next time
-        itsPreviousValue = new Double(dval);
-
-        return res;
+    // Get the new value as a double
+    double dval;
+    if (val instanceof Number) {
+      dval = ((Number) val).doubleValue();
+    } else if (val instanceof Angle) {
+      dval = ((Angle) val).getValue();
+    } else {
+      System.err.println("TranslationDelta: " + itsParent.getFullName() + ": Got NON-NUMERIC data!");
+      return new PointData(itsParent.getFullName());
     }
 
-    public static String[] getArgs()
-    {
-        return itsArgs;
+    PointData res = null;
+
+    // If we had a previous value we can calculate the change
+    if (itsPreviousValue != null) {
+      // Calculate the change in the value of the point
+      double delta = itsPreviousValue.doubleValue() - dval;
+      if (val instanceof Angle) {
+        // Watch for phase wrap
+        if (delta > Math.PI) {
+          delta = 2 * Math.PI - delta;
+        } else if (delta < -Math.PI) {
+          delta = 2 * Math.PI + delta;
+        }
+        res = new PointData(itsParent.getFullName(), Angle.factory(delta, Angle.Format.RADIANS));
+      } else {
+        res = new PointData(itsParent.getFullName(), new Double(delta));
+      }
+    } else {
+      // No previous value so can't calculate delta, so return null data
+      return null;
     }
+
+    // Record the current value for use next time
+    itsPreviousValue = new Double(dval);
+
+    return res;
+  }
+
+  public static String[] getArgs() {
+    return itsArgs;
+  }
 }
