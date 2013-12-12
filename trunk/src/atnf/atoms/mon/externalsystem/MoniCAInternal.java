338770bd5fddaf7fa142e7ff5061c072e0d99cdd
brodrick@gmail.com <brodrick@gmail.com@73e435cf-5d48-0410-ba38-3de2b9e16419>
2013-12-12 03:43:29 +0000

    Allow monitoring of server uptime.

--- trunk/src/atnf/atoms/mon/externalsystem/MoniCAInternal.java
+++ trunk/src/atnf/atoms/mon/externalsystem/MoniCAInternal.java
@@ -9,48 +9,56 @@
 package atnf.atoms.mon.externalsystem;
 
 import atnf.atoms.time.AbsTime;
+import atnf.atoms.time.Time;
 import atnf.atoms.mon.*;
 import atnf.atoms.mon.transaction.*;
 
 /**
  * Used to return data about the MoniCA server.
  * 
- * Points using this should have a TransactionStrings with the channel set to
- * "system" and a second argument which determines the data to be retrieved.
- * This may be one of the following:
+ * Points using this should have a TransactionStrings with the channel set to "system" and a second argument which determines the
+ * data to be retrieved. This may be one of the following:
  * 
  * <ul>
  * <li><b>time</b> Return the current time on the server.
  * <li><b>points</b> Return the current number of points defined on the server.
- * <li><b>systems</b> Return the current number of external systems defined on
- * the system.
+ * <li><b>systems</b> Return the current number of external systems defined on the system.
+ * <li><b>uptime</b> The elapsed time since the server was started.
  * </ul>
  * 
  * @author David Brodrick
  **/
 class MoniCAInternal extends ExternalSystem {
+  /** The time the server started. */
+  private AbsTime itsStartTime;
+  
   public MoniCAInternal(String[] args) {
     super("system");
+    
+    //Record the system start time
+    itsStartTime = new AbsTime();
   }
 
   protected void getData(PointDescription[] points) throws Exception {
-    try{
-    for (int i = 0; i < points.length; i++) {
-      PointDescription desc = points[i];
-      // Get the Transactions which associates the point with us
-      TransactionStrings thistrans = (TransactionStrings) getMyTransactions(desc.getInputTransactions()).get(0);
-      PointData pd = new PointData(desc.getFullName(), AbsTime.factory(), null);
+    try {
+      for (int i = 0; i < points.length; i++) {
+        PointDescription desc = points[i];
+        // Get the Transactions which associates the point with us
+        TransactionStrings thistrans = (TransactionStrings) getMyTransactions(desc.getInputTransactions()).get(0);
+        PointData pd = new PointData(desc.getFullName(), AbsTime.factory(), null);
 
-      if (thistrans.getString().equals("time")) {
-        pd.setData(pd.getTimestamp());
-      } else if (thistrans.getString().equals("points")) {
-        pd.setData(new Integer(PointDescription.getAllPoints().size()));
-      } else if (thistrans.getString().equals("systems")) {
-        pd.setData(new Integer(ExternalSystem.getAllExternalSystems().size()));
+        if (thistrans.getString().equals("time")) {
+          pd.setData(pd.getTimestamp());
+        } else if (thistrans.getString().equals("points")) {
+          pd.setData(new Integer(PointDescription.getAllPoints().size()));
+        } else if (thistrans.getString().equals("systems")) {
+          pd.setData(new Integer(ExternalSystem.getAllExternalSystems().size()));
+        } else if (thistrans.getString().equals("uptime")) {
+          pd.setData(Time.diff(new AbsTime(), itsStartTime));
+        }
+
+        desc.firePointEvent(new PointEvent(this, pd, true));
       }
-
-      desc.firePointEvent(new PointEvent(this, pd, true));
-    }
     } catch (Exception e) {
       e.printStackTrace();
     }
