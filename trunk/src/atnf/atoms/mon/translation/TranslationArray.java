a373a71db619019f00b5ebaedb3e2328cd992287
brodrick@gmail.com <brodrick@gmail.com@73e435cf-5d48-0410-ba38-3de2b9e16419>
2014-06-17 02:20:03 +0000

    Allow hex index.

--- trunk/src/atnf/atoms/mon/translation/TranslationArray.java
+++ trunk/src/atnf/atoms/mon/translation/TranslationArray.java
@@ -12,69 +12,60 @@ import atnf.atoms.mon.PointDescription;
 
 /**
  * Returns a particular entry from an array.
- * <P>A monitor point that uses this translation would normally have a
- * <i>Listen</i> transaction that listens to a monitor point whose data
- * is an array. This translation would then extract a single entry from the
- * array and output just that entry.
- *
+ * <P>
+ * A monitor point that uses this translation would normally have a <i>Listen</i> transaction that listens to a monitor point whose
+ * data is an array. This translation would then extract a single entry from the array and output just that entry.
+ * 
  * @author David Brodrick
  * @version $Id: $
  **/
-public class
-TranslationArray
-extends Translation
-{
+public class TranslationArray extends Translation {
   /** The array index we should return. */
   private int itsIndex = 0;
-   
-  protected static String[] itsArgs = new String[]{"Translation Array",
-  "Array", "Array index to return", "java.lang.Integer"};
-   
-  public
-  TranslationArray(PointDescription parent, String[] init)
-  {
+
+  protected static String[] itsArgs = new String[] { "Translation Array", "Array", "Array index to return", "java.lang.Integer" };
+
+  public TranslationArray(PointDescription parent, String[] init) {
     super(parent, init);
-    if (init.length!=1) {
-      System.err.println("ERROR: TranslationArray (for " + itsParent.getName()
-			 + "): Expect 1 Argument.. got " + init.length);
+    if (init.length != 1) {
+      System.err.println("ERROR: TranslationArray (for " + itsParent.getName() + "): Expect 1 Argument.. got " + init.length);
     } else {
-      itsIndex = Integer.parseInt(init[0]);
+      if (init[0].startsWith("0x")) {
+        // Allow a hex index
+        itsIndex = Integer.parseInt(init[0].substring(2), 16);
+      } else {
+        itsIndex = Integer.parseInt(init[0]);
+      }
     }
   }
-   
-  public
-  PointData
-  translate(PointData data)
-  {
-    //Precondition
-    if (data==null) {
+
+  public PointData translate(PointData data) {
+    // Precondition
+    if (data == null) {
       return null;
     }
 
-    //Get the full array
-    Object[] array = (Object[])data.getData();
+    // Get the full array
+    Object[] array = (Object[]) data.getData();
 
-    //Create the new data structure to be returned
+    // Create the new data structure to be returned
     PointData res = new PointData(itsParent.getFullName());
 
-    //If the data is null we need to throw a null-data result
-    if (array==null || array.length<itsIndex) {
+    // If the data is null we need to throw a null-data result
+    if (array == null || array.length < itsIndex) {
       return res;
     }
 
-    //Copy just the particular index that we've been asked to
+    // Copy just the particular index that we've been asked to
     res.setData(array[itsIndex]);
 
-    //Keep the time-stamp of the parent point rather than use "now"
+    // Keep the time-stamp of the parent point rather than use "now"
     res.setTimestamp(data.getTimestamp());
 
     return res;
   }
 
-  public static
-  String[]
-  getArgs()
-  {
+  public static String[] getArgs() {
     return itsArgs;
   }
 }
