4754610aaa802ddc20db0702348d8a1a74834b49
Malte Marquarding <Malte.Marquarding@csiro.au>
2016-03-21 16:27:05 +1100

    Add explict check for '.txt' suffix for file in points.d

--- src/atnf/atoms/mon/MoniCAMain.java
+++ src/atnf/atoms/mon/MoniCAMain.java
@@ -168,8 +168,8 @@ public class MoniCAMain {
           // Sort the file list lexicographically to enabled expected conf.d behaviour
           Arrays.sort(flist);
           for (File f : flist) {
-            if (!f.isFile()) {
-              theirLogger.warn("Points definition file \"" + f + "\" is not a file");
+	      if (!(f.isFile() && f.getName().endsWith(".txt"))) {
+              theirLogger.warn("Points definition file \"" + f + "\" is not a file or does not end in '.txt'");
             } else {
               // Looks like it should be a valid points definition file, so parse it
               theirLogger.info("Loading point definitions from \"" + f + "\"");
